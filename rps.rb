e8bd7035e05b3feaef096f89f0ec76b33744597d
Garrett <garrettweinert@gmail.com>
2015-06-29 14:31:32 -0700

    Working on 2 player game. Working on print out which player is going bug

--- rps.rb
+++ rps.rb
@@ -2,10 +2,11 @@ require 'pry-byebug'
 
 class Game
   def initialize
-    @player = Player.new
-    @computer = Computer.new #@AI reference CLass Computer
+    @player = Player.new("player1")
+    # @computer = Computer.new #@AI reference CLass Computer
     #prompt play to play
     prompt_player
+    play_against_computer?
     play
   end
 
@@ -33,21 +34,45 @@ class Game
 
   def prompt_player  #intro & rules
     puts "Rock, paper, scissors game!"
-    puts ">>"
+  end
+
+  def play_against_computer?
+    puts "Play against computer?"
+    print ">>"
+    input = gets.chomp
+    if input == "yes"
+      @player2 = Computer.new
+    else
+      @player2 = Player.new("player2")
+    end
+  end
+
+  def is_computer?
+    if @player2.class == Computer
+      return true
+    else
+      return false
+    end
   end
 
   def play
+
+
     p_choice = @player.get_input
-    c_choice = @computer.computer_choice
-    puts "Computer picks #{c_choice}."
+
+    if is_computer?
+      c_choice = @player2.get_input(@player)
+    else
+      c_choice = @player2.get_input(@player2)
+    end
+
+    puts "Player2 picks #{c_choice}."
     if tie?(p_choice, c_choice)
       puts "It's a tie!"
+    elsif does_p1_win?(p_choice, c_choice)
+      puts "Player1 wins!"
     else
-      if does_p1_win?(p_choice, c_choice)
-        puts "Player wins!"
-      else
-        puts "Player loses!"
-      end
+      puts "Player1 loses!"
     end
   end
 
@@ -55,13 +80,16 @@ end
 
 class Player
 
-  def initialize #??? do we need this?
+  def initialize(name) #??? do we need this?
+    @name = name
   end
 
-  def get_input #checks with get_input
+  def get_input(player) #checks with get_input
+    print "#{player.name}>>"
     input = gets.strip
     until valid_input? (input)
       puts "Please choose: rock, paper, or scissors."
+      print ">>"
       input = gets.chomp
     end
     input
@@ -81,10 +109,10 @@ end
 class Computer
 
   def initialize
-
+    @name = "computer"
   end
 
-  def computer_choice
+  def get_input
     choices = ["rock", "paper", "scissors"]
     choices.sample
   end
