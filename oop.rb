d91fae66cde5fda5d7b6c40bcae4746d3b4efb46
alokp <alokpra@gmail.com>
2015-06-29 09:57:19 -0700

    fibonacci my_benchmark complete

--- oop.rb
+++ oop.rb
@@ -1,3 +1,5 @@
+require 'time.rb'
+
 def reverse(str)
   ans = []
   str =  str.chars
@@ -7,4 +9,35 @@ def reverse(str)
   end
   ans.join
 
+end
+
+def fibonacci (num)
+	#count = 1
+	arr = [0,1]
+	# while count < num
+	# 	arr << arr[count] + arr[count-1]
+	# 	count += 1
+	# end
+	for i in 1..(num-2)
+		arr << arr[i] + arr[i-1]
+	end
+	arr
+	# why does it have to be num-1?
+end
+
+def my_benchmark(number_of_times, &block)
+	start_time = Time.now
+	number_of_times.times do
+		yield
+	end
+	end_time = Time.now
+	end_time - start_time
+	# how do we not puts?
+end
+
+class Array
+	# att_accessor: @var
+	def my_each(&block)
+		self
+	end
 end
