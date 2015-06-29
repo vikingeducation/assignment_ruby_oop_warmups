eeecab4e19d385977416f3b2b5444fae98b4f85d
Donald Kelsey <dmkelseyii@gmail.com>
2015-06-29 12:54:00 -0500

    Finished rps

--- rps.rb
+++ rps.rb
@@ -22,91 +22,97 @@
 #     RNG
 
 
-
+require 'io/console'
 
 class GameState
 
-  def initialize(oppenent = 'computer')
+  def initialize(opponent = 'computer')
 
     @player1 = Human.new
 
-    @player2 = Computer.new
+    if opponent == 'computer'
+      @player2 = Computer.new
+    else
+      @player2 = Human.new
+    end
 
   end
 
   def start_game
-
     puts "Input 'R' for Rock, 'P' for Paper, or 'S' for Scissors"
-
   end
-
-
-
 
   def get_moves
 
-    puts "Player 1: Enter Move"
+    words = {"S" => "Scissors", "R" => "Rock", "P" => "Paper"}
 
+    puts "Player 1: Enter Move"
     p1 = @player1.move
 
-    puts "Player 2: Enter Move"
-
+    puts "Player 2: Enter Move" if @player2.is_a? Human
     p2 = @player2.move
+
+    puts "Player 1 played #{words[p1]} and Player 2 played #{words[p2]}"
 
     return [p1, p2]
 
   end
 
+  def p1_plays_s(p2_move)
+      if p2_move == 'P'
+        return 1
+      elsif p2_move == 'R'
+        return 2
+      end
+  end
+
+  def p1_plays_r(p2_move)
+      if p2_move == 'S'
+        return 1
+      elsif p2_move == 'P'
+        return 2
+      end
+  end
+
+  def p1_plays_p(p2_move)
+      if p2_move == 'R'
+        return 1
+      elsif p2_move == 'S'
+        return 2
+      end
+  end
+
   def determine_winner(arr)
 
     p1_move = arr[0]
-
     p2_move = arr[1]
 
-    if p1_move == p2_move
-
-      return 0
-
-    end
+    return 0 if p1_move == p2_move
 
     if p1_move == 'S'
-
-      if p2_move == 'P'
-
-        return 1
-
-      elsif p2_move == 'R'
-        
-        return 2
-
-      end
+      p1_plays_s(p2_move)
 
     elsif p1_move == 'R'
-
-      if p2_move == 'S'
-
-        return 1
-
-      elsif p2_move == 'P'
-
-        return 2
-
-      end
+      p1_plays_r(p2_move)
 
     elsif p1_move == 'P'
+      p1_plays_p(p2_move)
+    end
 
-      if p2_move == 'R'
+  end
 
-        return 1
+  def print_result(winner)
+    if winner == 0
+      puts "The game is a tie!"
+    else
+      puts "The winner is Player #{winner}"
+    end
+  end
 
-      elsif p2_move == 'S'
-
-        return 2
-
-      end
-
-    end 
-
+  def quit?
+    puts "Play again? Type 'Y' to play again. Type anything else to quit."
+    selection = gets.chomp.upcase
+    selection == "Y" ? false : true
   end
 
   def play
@@ -114,62 +120,62 @@ class GameState
     start_game
 
     while true
-
       current_moves = get_moves
+      winner = determine_winner(current_moves)
+      print_result(winner)
 
-      winner = determine_winner(current_moves) 
-
-      puts "The winner is Player #{winner}"
-
-      break
-
+      break if quit?
     end
 
+    puts "Thanks for playing!"
   end
 
 end
-
-
 
 
 class Player
 
-
   def move
-
     pass
-
   end
-
 
 end
 
 
-class Human << Player 
+class Human < Player
 
   def move
-
     selection = ""
 
+    # Don't let the player put in bad moves
     until ["S","P","R"].include?(selection)
-
-      selection = gets.chomp.upcase
-
-    end 
+      selection = STDIN.noecho(&:gets).chomp.upcase
+      unless ["S","P","R"].include?(selection)
+        puts "Invalid choice, please try again:"
+      end
+    end
 
     return selection
-
   end
 
 end
 
+class Computer < Player
 
+  def move
+    comp_move = ["Scissors","Paper","Rock"].sample
+    # puts "The computer plays #{comp_move}!"
+    comp_move = comp_move[0]
+    return comp_move
+  end
 
+end
 
+#game = GameState.new
 
+game = GameState.new
 
-
-
+game.play
 
 
 
--- warmups.rb
+++ warmups.rb
@@ -3,15 +3,11 @@ def my_reverse (str)
   arr = str.split('')
 
   result = []
-
   until arr.empty?
-
     result << arr.pop()
-
   end
 
   return result.join('')
-
 
 end
 
@@ -22,30 +18,20 @@ end
 def fib_helper(num)
 
   if num == 0
-
     return 0
-
   elsif num == 1
-
     return 1
-
-  else 
-
-    return fib_helper(num - 1) + fib_helper(num - 2) 
-
+  else
+    return fib_helper(num - 1) + fib_helper(num - 2)
   end
 
 end
 
 def fibs (num)
 
-
   arr = []
-
   num.times do |value|
-
     arr.push(fib_helper(value))
-
   end
 
   return arr
@@ -58,24 +44,81 @@ end
 def my_benchmark(num)
 
   start_time = Time.now
-
-  num.times do 
-
+  num.times do
     yield
-
   end
 
   Time.now - start_time
-
 
 end
 
 
 
-puts my_benchmark(10000) {puts "hi"}
+# puts my_benchmark(10000) {puts "hi"}
 
+class Array
 
+  def my_each
+    i = 0
+    while !self[i].nil?
+      yield self[i]
+      i += 1
+    end
+    return self
+  end
 
+  def my_map
+    result = []
+    self.my_each do |val|
+      result << yield(val)
+    end
+    return result
+  end
 
+  def my_select
+    result = []
+    self.my_each do |val|
+      if yield(val)
+        result << val
+      end
+    end
+    return result
+  end
 
+  def my_all?
+    self.my_each do |val|
+      if !yield(val)
+        return false
+      end
+    end
+    return true
+  end
 
+  # Memo = running total
+  # item = current val
+
+  def my_inject(arg = 0)
+    memo = arg
+    self.my_each do |item|
+      memo = yield(memo, item)
+    end
+
+    return memo
+
+  end
+
+end
+
+#[1,2,5].my_each {|item| puts item}
+
+#my_proc = Proc.new{|item| puts item ** 2}
+
+#[1,2,5].my_each(&my_proc)
+
+#p [1,2,5].my_map {|item| item ** 2}
+
+#p [1,2,5].my_select{|item| item.even?}
+
+#p [4,2,6].my_all?{|item| item.even?}
+
+p [1,2,5].my_inject(0) {|memo, item| memo + item}
