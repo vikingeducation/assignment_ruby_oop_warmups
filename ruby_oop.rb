72c32b5f8e0af04f1d0460957d05641fd76947b0
Alice Fung <alice.o.fung@gmail.com>
2015-06-29 12:37:11 -0400

    add fibs and my_benchmark methods

--- ruby_oop.rb
+++ ruby_oop.rb
@@ -10,4 +10,27 @@ end
 # my_reverse("garrett")
 
 
+def fibs(num)
+  fib_array = [0,1]
+  if num == 1
+    fib_array[0].to_a
+  elsif num == 2
+    fib_array
+  else num > 2
+    (num-2).times do
+      fib_array.push(fib_array[-1] + fib_array[-2])
+    end
+  end
+  fib_array
+end
+ #p fibs(8)
 
+def my_benchmark(num_times)
+  t1= Time.now
+  num_times.times do
+    yield
+  end
+  time = Time.now - t1
+end
+
+puts my_benchmark(10000) { puts "hi" }
