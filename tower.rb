168ca5667a6f2261b0910ba5cba7996f3720c374
Alice Fung <alice.o.fung@gmail.com>
2015-06-29 17:56:22 -0400

    start tower outline

---
+++ tower.rb
@@ -0,0 +1,29 @@
+class Tower
+
+end
+
+class Player
+
+  def initialize
+  end
+
+  def get_input
+
+  end
+
+  def input_valid?
+
+  end
+end
+
+class Game
+
+  def initialize
+    #rules
+    #
+  end
+
+  def play
+  end
+
+end
