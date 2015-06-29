eb0c976cdf89312977dd9d56a159d3f9ea3f39e2
facingsouth <xin.d.liu@gmail.com>
2015-06-29 10:15:10 -0700

    small change

--- warmup.rb
+++ warmup.rb
@@ -16,3 +16,13 @@ def my_benchmark(ntimes)
   ntimes.times {yield}
   Time.now - start_time
 end
+
+class Array
+  def my_each
+    i = 0
+    while i <self.size
+      yield(self[i])
+      i += 1
+    end
+  end
+end
