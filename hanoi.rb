cd41a9979377353c174caa78fc549afcb7fbf659
Donald Kelsey <dmkelseyii@gmail.com>
2015-06-29 14:10:36 -0500

    Lunch break, partway through tower

--- hanoi.rb
+++ hanoi.rb
@@ -1,57 +1,65 @@
-class GameState(height)
+# This class is in charge of maintaining and manipulating the game state.
+class GameState
 
   attr_reader :c1, :c2, :c3
 
-def initialize
+  def initialize(height)
 
-  @height = height
+    @height = height
+    @col1, @col2, @col3 = (1..height).to_a.reverse, [], []
 
-  @col1, @col2, @col3 = (1..height).to_a.reverse, [], []
+    @win_condition = (1..height).to_a.reverse
 
-  @win_condition = (1..height).to_a.reverse
-
-  @colmap = {1 => @col1, 2 => @col2, 3 => @col3}
-
-end
-
-
-
-  def move(c1, c2)
-
-    if @colmap[c1].length == 0
-      puts "There is nothing to take from Column #{c1}"
-
-    elsif @colmap[c1][-1] > @colmap[c2][-1]
-      puts "That is an invalid move"
-
-    else
-      @colmap[c2] << @colmap[c1].pop  
-
-    end
-
+    # This colmap is for easier access to columns.
+    @colmap = {1 => @col1, 2 => @col2, 3 => @col3}
 
   end
 
 
+  # Takes a valid input and checks if the move is legal.
+  # Then, makes a move.
+  def move(c1, c2)
+    @colmap[c2] << @colmap[c1].pop
+  end
 
   def check_win?
-
     @col3 == @win_condition
-
   end
 
+  # This method is going to check if the move is legal and valid
+  # c1 is origin column, c2 is destination column.
+  def validate_input(c1,c2)
+    if @colmap[c1].length == 0
+      puts "There is nothing to take from Column #{c1}"
+    elsif @colmap[c1][-1] > @colmap[c2][-1]
+      puts "That is an invalid move"
+    else
+    end
+  end
 
+  def get_input()
+    c1, c2 = 0
+    loop do
+      puts "Enter origin column."
+      c1 = gets.to_i
+      break if (c1 < 4 && c1 > 0)
+    end
 
+    loop do
+      puts "Enter destination column."
+      c2 = gets.to_i
+      break if (c2 < 4 && c2 > 0)
+    end
 
-
-
+    validate_input(c1, c2)
+  end
 
 end
 
-
-
+# This class is in charge of representing the game state to the player.
 class Drawer
 
+  # This class just prints the board state out as arrays.
   def print_board(c1, c2, c3)
 
     p (c1)
@@ -61,38 +69,35 @@ class Drawer
     p (c3)
 
   end
-
-
 end
 
-
-class MoveParser
-
-end
-
-
+# This class represents the game as a whole.
+# It has its own drawer, move parser, and game state.
 class Game
 
   def initialize(height = 3)
 
     @drawer = Drawer.new
 
-    @parser = MoveParser.new
-
     @game_state = GameState.new(height)
 
   end
 
-
+  # The main game loop is here.
   def play
 
-#loop goes here
+    while true
 
-  current_move = @parser.get_valid_move
+      @gamestate.get_input
 
-  @gamestate.move(current_move)
+      @gamestate.move(current_move)
 
-  @drawer.print_board(@gamestate.c1, @gamestate.c2, @gamestate.c3)
+      @drawer.print_board(@gamestate.c1, @gamestate.c2, @gamestate.c3)
+    end
+
+  end
+
+  def get_input
 
   end
 
