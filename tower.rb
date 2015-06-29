e4713b71025fc65a2da948344b55f89b1d03a355
Garrett <garrettweinert@gmail.com>
2015-06-29 15:29:38 -0700

    Started OOP TOH Game

--- tower.rb
+++ tower.rb
@@ -1,5 +1,13 @@
 class Tower
 
+  def initialize(tower = 0)
+    @tower = tower
+  end
+
+  def render
+  end
+
+
 end
 
 class Player
@@ -8,10 +16,28 @@ class Player
   end
 
   def get_input
+    input = gets.chomp
+    from_tower = input[1]
+    to_tower = input[3]
 
+    unless input_valid?(from_tower, to_tower)
+      input = gets.chomp
+    end
+
+    from_tower = input[1]
+    to_tower = input[3]
+
+    return_array = [from_tower, to_tower]
+    return_array
   end
 
-  def input_valid?
+  def input_valid?(from_tower, to_tower)
+    if (1..3) === from_tower || (1..3) === to_tower
+      print "its valid"
+      return true
+    else 
+      return false
+    end
 
   end
 end
@@ -20,10 +46,49 @@ class Game
 
   def initialize
     #rules
+    # puts "Welcome to Tower of Hanoi"
+    # puts "Please Enter moves [1, 3]"
+    # puts "Enter how many disks you want to play"
+    # print ">>"
+    input = gets.chomp
+    @left_tower = Tower.new(input)
+    @mid_tower = Tower.new
+    @right_tower = Tower.new
+    @goal_tower_amount = Tower.new(input)
+    @player = Player.new
+
+
+
     #
   end
 
-  def play
+  def can_move?()
+    # if 
   end
+
+  def game_won?
+
+    if @right_tower == @goal_tower_amount
+      return true
+    else
+      return false
+    end
+
+  end
+
+  def play
+
+    unless game_won?
+      print ">>"
+      moves = @player.get_input
+      can_move?(moves[0], moves[1])
+
+
+    end
+
+
+  end
+
+
 
 end
