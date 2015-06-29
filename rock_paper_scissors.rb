77a8aeae19819bcc3a716a8189cef91db234c291
alokp <alokpra@gmail.com>
2015-06-29 12:01:25 -0700

    player vs. computer complete

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,52 +1,53 @@
-=begin 
-    
-    - Rock Paper sissors class - with method "main" which runs the program
+=begin
+    - Rock Paper Scissors class - with method "main" which runs the program
     - first class , asking players choice?
     - 2nd class computer choice
     - a method in RockPaperSissors compare the 2 inputs
     - method to return winner
-
 =end
 
 class RockPaperScissors
- 
   def main
-
-    player = PlayerChoice.new
-    computer = ComputerChoice.new
-
+    @player = PlayerChoice.new
+    @computer = ComputerChoice.new
+    compare(@player.choice, @computer.choice)
   end
 
-  def compare
-
+  def compare(player, computer)
+    puts "The computer picked: #{computer}"
+    if (player == computer)
+      puts "tie"
+    elsif (player == "rock") && (computer == "paper")
+      puts "computer wins"
+    elsif (player == "rock") && (computer == "scissors")
+      puts "player wins"
+    elsif (player == "paper") && (computer == "rock")
+      puts "player wins"
+    elsif (player == "paper") && (computer == "scissors")
+      puts "computer wins"
+    elsif (player == "scissors") && (computer == "rock")
+      puts "computer wins"
+    elsif (player == "scissors") && (computer == "paper")
+      puts "computer wins"
+    end
   end
-
-  def winner
-
-  end
-
 end
 
 class PlayerChoice
-  attr_accessor :player_choice
-  
-  def intialize
-    puts "what is your weapon? "
-    @player_choice = gets.chomp
+  attr_accessor :choice
+  def initialize
+    puts "what is your weapon? (rock, paper, scissors)"
+    @choice = gets.chomp
+    @choice.downcase!
   end
-
 end
 
 class ComputerChoice
-  attr_accessor :computeR_choice
-  
-  def intialize
+  attr_accessor :choice
+  def initialize
     num = rand(3)
     rps = ["rock", "paper" , "scissors"]
-
-      @computer_choice = rps[num]
-
+    @choice = rps[num]
   end
-  
 end
 
