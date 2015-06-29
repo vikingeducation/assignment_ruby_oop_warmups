64927a8005d092789bc891baaa2684346c2eb70c
michelletoussaint <toussaint.michelle@gmail.com>
2015-06-29 13:14:50 -0400

    Completed my_each

--- my_each.rb
+++ my_each.rb
@@ -1,1 +1,20 @@
-class MyEach < Array
+class Array
+
+  def my_each 
+
+    i = 0
+
+    while i < self.length
+
+      puts yield (self[i])
+
+      i+=1
+
+    end
+
+  end
+
+end
+
+puts [1, 2, 3].my_each {|item| item**2}
+
