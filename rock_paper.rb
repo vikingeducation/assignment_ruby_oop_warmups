8851f6507240c0943893dfec35491b71506a0771
facingsouth <xin.d.liu@gmail.com>
2015-06-29 16:43:25 -0700

    add hanoi tower

--- rock_paper.rb
+++ rock_paper.rb
@@ -7,10 +7,14 @@ class RockPaper
       #Choose the game type
       game_type
 
+      #break if @@second_player_move == "quit"
+
       #Player 1
-      get_an_answer=="quit" ? @@your_move = get_an_answer : break
+      @your_move = get_an_answer
+
+      #break if @@your_move == "quit"
         
-      puts "The other player plays #{@@second_player_move}"
+      puts "The other player plays #{@second_player_move}"
 
       winner     
     end
@@ -25,12 +29,12 @@ class RockPaper
   end
 
   def play_vs_computer
-    @@second_player_move = @@options.key(rand(3))
+    @second_player_move = @@options.key(rand(3))
   end
 
   def play_vs_human
     puts "Player two, please enter your move"
-    @@second_player_move = get_an_answer
+    @second_player_move = get_an_answer
   end
 
   def get_an_answer
@@ -39,8 +43,10 @@ class RockPaper
         puts "Enter quit if you want to end the game."
         answer = gets.chomp
 
-        if ["rock", "paper", "scissors","quit"].include?(answer)
+        if ["rock", "paper", "scissors"].include?(answer)
           return answer
+        elsif answer == "quit"
+          exit
         else
           puts "Your input is not valid"
           next
@@ -50,9 +56,9 @@ class RockPaper
   end
 
   def winner
-    if @@your_move == @@second_player_move
+    if @your_move == @second_player_move
         puts "Tie"
-    elsif [1,-2].include?(@@options[@@your_move] - @@options[@@second_player_move])
+    elsif [1,-2].include?(@@options[@your_move] - @@options[@second_player_move])
       puts "Congrats! You win."
     else
       puts "You lose!"
