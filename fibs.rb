97618f81b592223f081e733ad915a43b8e9cdee9
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-06-29 12:41:18 -0400

    fibs.rb created

---
+++ fibs.rb
@@ -0,0 +1,31 @@
+def fibs(int)
+
+	return_array = []
+
+	if int == 1
+
+		return_array << 0
+
+	elsif int == 2
+
+		return_array << 0
+		return_array << 1
+
+	elsif int > 2
+
+		return_array << 0
+		return_array << 1
+
+		(2..int - 1).each do |i|
+
+			return_array << return_array[i - 1] + return_array[i - 2]
+
+		end
+
+	end
+
+	return_array
+
+end
+
+print fibs(8)
---
+++ my_reverse.rb
@@ -0,0 +1,5 @@
+def my_reverse(str)
+	return str[-1]
+end
+
+puts my_reverse("hello")
