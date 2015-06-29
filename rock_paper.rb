bfc6a1c9b9c6d5b2f50326778358a42822cea4a9
ayva <olga.ayva@gmail.com>
2015-06-29 14:10:07 -0700

    rock_paper hash added

--- rock_paper.rb
+++ rock_paper.rb
@@ -1,17 +1,20 @@
 class RockPaper
-  @@options = ["rock", "paper", "scissors"]
+  @@options = {"rock" => 0, "paper"=> 1, "scissors"=> 2}
 
   def begin
     while true
       puts "Please enter your move (rock, paper or scissors)"
       puts "Enter quit if you wat to end the game."
       @@your_move = gets.chomp
+      
       break if @@your_move == "quit"
-      @@second_player_move = @@options.shuffle[0]
-      if @@your_move == @@second_player_move
+      
+      @@second_player_move = rand(3)
+      
+      if @@your_move == @@second_player_move 
         puts "Tie"
-        next
       end
+        
       if win?
         puts "Congrats! You win."
       else
@@ -21,22 +24,10 @@ class RockPaper
   end
 
   def win?
-    if @@your_move == "rock"
-      if @@second_player_move == "paper"
-        return false
-      else
+    if [-1,2].include?(@@options[@@your_move] - @@second_player_move)
         return true
-    elsif @@your_move == "paper"
-      if @@second_player_move == "scissors"
+    else 
         return false
-      else
-        return true
-    else
-      if @@second_player_move == "rock"
-        return false
-      else
-        return true
-      end
     end
   end
 end
