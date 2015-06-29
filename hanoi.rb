3583f143047d9374a8befec5656d62b6857f85e0
mikebutts <mgbutts@gmail.com>
2015-06-29 19:29:36 -0400

    working hanoi

--- hanoi.rb
+++ hanoi.rb
@@ -15,6 +15,12 @@ class Toh
 		tower = height.tower
 		show = Display.new
 		show.create_display(tower)
+    move = Validate.new(tower)
+
+    
+      move.move_choice
+
+
 	end
 end
 
@@ -23,7 +29,7 @@ class HeightOfTower
 	def initialize
 		puts "What is the height of your tower?"
 		height = gets.chomp
-		@tower = {:t1 => (1..height.to_i).to_a, :t2 => [], :t3 => []}
+		@tower = {"t1" => (1..height.to_i).to_a, "t2" => [], "t3" => []}
 	end
 end
 
@@ -40,6 +46,30 @@ class Display
 end
 
 class Validate
+   
+   def initialize(tower)
+      @tower = tower
+   end
+
+   def move_choice
+    #until valid_entry && valid_move
+      puts "Please enter your move in [from , to]"
+      @entered = gets.chomp.split(",").map{|x| x.to_i}
+      
+       valid_move
+    #end
+   end
+
+    def valid_entry
+      @entered.all?{|x| (1..3).include?(x)}
+    end
+
+  #
+    def valid_move
+    
+      puts @tower["t#{@entered[0]}"]
+    end 
+
 
 end
 
