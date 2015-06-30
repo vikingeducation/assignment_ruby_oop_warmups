d66fbea7028816dc76a08e9adfac397cba700e2a
Alice Fung <alice.o.fung@gmail.com>
2015-06-29 20:34:42 -0400

    fixed bugs

--- tower.rb
+++ tower.rb
@@ -29,7 +29,7 @@ class Player
     puts from_tower
     puts to_tower
 
-    unless input_valid?(from_tower, to_tower)
+    until input_valid?(from_tower, to_tower)
       input = gets.chomp
     end
 
@@ -41,7 +41,7 @@ class Player
   end
 
   def input_valid?(from_tower, to_tower)
-    if (1..3) === from_tower || (1..3) === to_tower
+    if (1..3).include?(from_tower) && (1..3).include?(to_tower)
       print "its valid"
       return true
     else
@@ -55,7 +55,7 @@ end
 class Game
 
   def initialize
-    # binding.pry
+ #    binding.pry
     #rules
     # puts "Welcome to Tower of Hanoi"
     # puts "Please Enter moves [1, 3]"
@@ -75,24 +75,39 @@ class Game
 
   def can_move?(from, to)
     good_move = true
-    good_move = false if @board[from-1].empty?   # if from is empty
+    if @board[from-1].empty?   # if from is empty
+      good_move = false
+
+    end
     # if disk is bigger than disk on to_tower
-    good_move = false if @board[from-1][-1] > @board[to-1][-1]
+    if @board[to-1].empty?
+      good_move = true
+    elsif (@board[from-1][-1] > @board[to-1][-1])
+      good_move = false
+    end
     good_move
   end
 
   def game_won?
 
-    if @right_tower == @goal_tower_amount
+    if @right_tower.tower == @goal_tower_amount.tower
+      return true
+    elsif @mid_tower.tower == @goal_tower_amount.tower
       return true
     else
-      return false
+      false
     end
+
+    # if @right_tower.inject == @goal_tower_amount.inject || (@mid_tower.inject == @goal_tower_amount.inject)
+    #   return true
+    # else
+    #   false
+    # end
 
   end
 
   def move_disk (from, to)
-    @board[to-1] << @board[from-1][-1].pop
+    @board[to-1] << @board[from-1].pop
   end
 
   def render
@@ -121,10 +136,10 @@ class Game
 
   def play
     render
-    unless game_won?
+    until game_won?
       print ">>"
       moves = @player.get_input
-      unless can_move?(moves[0], moves[1])
+      until can_move?(moves[0], moves[1])
         print "Move is invalid. Please enter valid move >>"
         moves = @player.get_input
       end
