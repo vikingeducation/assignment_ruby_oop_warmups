55f9630c8396ec4283a6e7bf46b981311242efca
Julia Herron <julia.herron@gmail.com>
2016-01-04 10:04:40 -0800

    fibs method

--- ruby_methods.rb
+++ ruby_methods.rb
@@ -10,4 +10,19 @@ def my_reverse(string)
 end
 
 
-my_reverse("blah")
+
+def fib_rec(int)
+  return int if (0..1).include? int
+  (fib_rec(int-1) + fib_rec(int-2))
+end
+
+
+
+def fibs(int)
+  arr = []
+  (0..int).each do |number| 
+    arr.push(fib_rec(number))
+  end
+  return arr
+
+end
