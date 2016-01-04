f8f4b0b99fce240d720ad723a15548dc7724cde0
Andrew Baik <and.baik@gmail.com>
2016-01-04 18:03:58 -0500

    add valid_move? and general game loop

--- rps.rb
+++ rps.rb
@@ -6,24 +6,31 @@ class Player
 	end
 
 	attr_accessor :move
-#process player input (take text and make it the selection)
-#define all possible moves
+	#process player input (take text and make it the selection)
+	#define all possible moves
 
-
-	def valid_input?
-		@move = gets.chomp.upcase
-		if @move == "R" || @move == "ROCK"
+	#checks "move" argument if it's valid move
+	def valid_input?(move)
+		if move == "R" || move == "ROCK"
 			return true
-		elsif @move == "P" || @move == "PAPER"
-			return true	
-		elsif @move == "S" || @move == "SCISSORS"
+		elsif move == "P" || move == "PAPER"
+			return true
+		elsif move == "S" || move == "SCISSORS"
 			return true
 		else
-			print "Not a valid input\n"
 			false
 		end
 	end
 
+	#
+	def get_move
+		user_input = gets.chomp.upcase
+		if valid_input?(user_input)
+			@move = user_input
+		else
+			print "Not a valid input!\n"
+		end
+	end
 end
 
 class Game
@@ -31,20 +38,55 @@ class Game
 	def initialize
 		@player1 = Player.new
 		@player2 = Player.new
-		@turn = 1
+		@turn = true
+		@win = false
 	end
-#instruction
-#display game
-#game loop (while)
-#win conditions
+	#instruction
+	#display game
+	#game loop (while)
+	#win conditions
+
+	#prints welcome
 	def print_welcome
 		print "Welcome to RPSE!!!! Rock Paper Scissors Extreme!!!\n"
 	end
 
+	#print instructions
 	def print_instructions
 		print "Type your selection (ie., 'rock' or 'r'). Options include: (R)ock, (P)aper, (S)cissors\n"
 	end
 
+	#runs game logic
+	def game_loop
+
+		print_welcome
+		print_instructions
+
+		until @win
+
+			if @turn
+				puts "Player 1's Turn: "
+				@player1.get_move
+
+				@turn = false
+			else
+				puts "Player 2's Turn: "
+				@player2.get_move
+				@turn = true
+			end
+
+		end
+
+	end
+
+	#compares moves between players
+	def compare_moves(p1_move, p2_move)
+		case
+		when p1_move
+		when
+		when
+		end
+	end
 end
 
 class Computer
