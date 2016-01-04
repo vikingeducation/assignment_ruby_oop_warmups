e2369df1e976fea32e72224d5c370d048eea05bc
Thomas Lo <wagaiznogoud@gmail.com>
2016-01-04 10:47:01 -0800

    finish my_select method - warmup3

--- warmup3.rb
+++ warmup3.rb
@@ -34,11 +34,29 @@ class Array
 
 		return result_array
 	end
+
+	def my_select(theproc = nil)
+
+		result_array = []
+
+		if block_given?
+			self.my_each do |num|
+				result_array << num if yield(num)
+			end
+		else
+			self.my_each do |num|
+				result_array << num if theproc.call(num)
+			end
+		end
+
+		return result_array
+	end
 end
 
-my_proc = Proc.new { |item| puts item }
-
+my_proc = Proc.new { |item| item.even? }
+# puts [1,2,5].my_select(my_proc).inspect
+puts [1,2,5,9,8].my_select{ |item| item.even? }.inspect
 #[1,2,5].my_each(my_proc)
 #[1,2,5].my_each{|item| puts item**2}
-print [1,2,5].my_map{|item| item**2}
-print [1,2,5].my_map(my_proc)
+# print [1,2,5].my_map{|item| item**2}
+# print [1,2,5].my_map(my_proc)
