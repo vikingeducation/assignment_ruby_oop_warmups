17f4436cc036effb0e85cff3647720299b8e9bfb
Jeffrey Gisin <jgisin@gmail.com>
2016-01-04 11:17:48 -0700

    My_each finished - warmup3

--- warmup3.rb
+++ warmup3.rb
@@ -1,13 +1,25 @@
 class Array
-  def my_each
-    counter = 0
-    while counter < self.count
-      my_proc.call(self[counter])
-      counter += 1
-    end
-  end
+    def my_each(theproc = nil)
+
+	  	counter = 0
+
+	  	if block_given?
+			while counter < self.count
+		      yield(self[counter])
+		      counter += 1
+		    end
+		else
+		    while counter < self.count
+		      theproc.call(self[counter])
+		      counter += 1
+		    end
+		end
+
+		return self
+	end
 end
 
 my_proc = Proc.new { |item| puts item }
 
 [1,2,5].my_each(my_proc)
+[1,2,5].my_each{|item| puts item**2}
