885fbd1e5b7f2ebc63d737c4fbf0e5640bd23b7c
Sam O'Keefe <samok13@gmail.com>
2016-01-04 11:28:08 -0800

    Fibs attempts

--- fibs.rb
+++ fibs.rb
@@ -1,12 +1,32 @@
 def fibs(number)
   fibonacci_array = Array.new
   if number == 0
-    return [0]
+    fibonacci_array=[0]
   elsif number == 1
-    return [0, 1]
+    fibonacci_array[1]
   else
     new_element = fibs(number-1) + fibs(number-2)
     fibonacci_array += new_element
   end
   fibonacci_array
+end
+
+print fibs(1)
+print fibs(2)
+print fibs(3)
+
+# Think about splitting the recursive into its own method
+
+def fibs(number)
+fibonacci_array = [0,1]
+  
+    def generate_next(array, number) 
+      next_element = array[number-1]+array[number-2]
+      return next_element
+    end
+
+    (length-2).times do |element|
+      fibonacci_array << next_element(fibonacci_array)
+    end
+
 end
