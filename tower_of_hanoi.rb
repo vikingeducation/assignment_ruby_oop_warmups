1918861380895256a9d068d47e00d57a725fd8e9
koziscool <kozjob@yahoo.com>
2016-01-04 13:14:11 -0600

    first commit tower

--- tower_of_hanoi.rb
+++ tower_of_hanoi.rb
@@ -1,1 +1,42 @@
 # Tower Of Hanoi
+
+class TowerOfHanoi
+    def initialize (height = 3)
+        @height = height
+        @peg1 = []
+        @peg2 = []
+        @peg3 = []
+        @winner_peg = []
+
+       @height.downto 1 do | i |
+            @peg1 << i
+            @winner_peg << i
+        end
+        @peg_hash = { 1 =>@peg1, 2=>@peg2, 3=>@peg3 }
+    end
+
+    def display_game_state
+        @peg_hash.values
+        
+        output  = []
+        (@height - 1).downto 0 do | i |
+            line = []
+            @peg_hash.each do | key, peg |
+                value = peg[ i ] || 0
+                character = ""
+                character += ("-" * value)
+                character = character.ljust @height
+                line << character
+            end
+            output << line.join("|")
+        end
+        output.join("\n")
+    end
+
+
+
+end
+
+t = TowerOfHanoi.new(7)
+puts t.display_game_state
+
