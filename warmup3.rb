1dd457a1076c1167de241319806984c6289f5f2f
Jeffrey Gisin <jgisin@gmail.com>
2016-01-04 11:35:19 -0700

    my_map completed - warmup3

--- warmup3.rb
+++ warmup3.rb
@@ -17,9 +17,28 @@ class Array
 
 		return self
 	end
+
+	def my_map(theproc = nil)
+
+		result_array = []
+
+		if block_given?
+			self.my_each do |num|
+				result_array << yield(num)
+			end
+		else
+			self.my_each do |num|
+				result_array << theproc.call(num)
+			end
+		end
+
+		return result_array
+	end
 end
 
 my_proc = Proc.new { |item| puts item }
 
-[1,2,5].my_each(my_proc)
-[1,2,5].my_each{|item| puts item**2}
+#[1,2,5].my_each(my_proc)
+#[1,2,5].my_each{|item| puts item**2}
+print [1,2,5].my_map{|item| item**2}
+print [1,2,5].my_map(my_proc)
