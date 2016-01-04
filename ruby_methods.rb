ecea5a3d2e1bd9baf287773d33aab2c1b87ebe84
sunsplat <heeycutie@yahoo.com>
2016-01-04 13:40:26 -0500

    enumerables complete

--- ruby_methods.rb
+++ ruby_methods.rb
@@ -26,3 +26,29 @@ def fibs(int)
   return arr
 
 end
+
+
+def my_benchmark(number_of_times)
+  start_time = Time.now
+  number_of_times.times do 
+    yield
+  end
+  end_time = Time.now
+  end_time - start_time
+end
+
+my_benchmark(1) { "hi" }
+my_benchmark(10000) { "hi" }
+my_benchmark(1000000) { "hi" }
+
+class Array
+  def my_each
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1    
+    end
+  end
+end
+[1,2,5].my_each{ |item| puts item }
+
