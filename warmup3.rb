70ec237066016235f3b2668cb1c616f1e3e0bf22
Andrew Baik <and.baik@gmail.com>
2016-01-04 14:27:35 -0500

    working on my_inject

--- warmup3.rb
+++ warmup3.rb
@@ -67,24 +67,32 @@ class Array
 		return true
 	end
 
-	def my_inject(theproc = nil)
-		total = 0
+	def my_inject(theproc = nil, init = nil)
+		init ? total = init : total = 0
 		if block_given?
 			self.my_each do |num|
-				yield(total, num)
+				total = yield(total, num)
 			end
 		else
 			self.my_each do |num|
-
+        total = theproc.call(total, num)
 			end
 		end
+    total
 	end
+
 end
 
-my_proc = Proc.new { |item| item.even? }
-# puts [1,2,5].my_select(my_proc).inspect
-puts [8,2,6,12].my_all?{ |item| item.even? }.inspect
+#my_proc = Proc.new { |item| item.even? }
+
 #[1,2,5].my_each(my_proc)
 #[1,2,5].my_each{|item| puts item**2}
 # print [1,2,5].my_map{|item| item**2}
 # print [1,2,5].my_map(my_proc)
+# puts [1,2,5].my_select(my_proc).inspect
+# puts [8,2,6,12].my_all?{ |item| item.even? }.inspect
+my_proc = Proc.new { |memo, item| memo + item }
+#puts [1,2,5].my_inject(my_proc)
+#puts [1,2,5].my_inject(my_proc, 0)
+#puts [1,2,5].my_inject(0) { |memo, item| memo + item }
+puts [1,2,5].inject(0, &my_proc)
