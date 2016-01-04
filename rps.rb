118fe2042a63d5d04380c18e1fd63f61b6223bec
Deepa Kackar <dkackar99@yahoo.com>
2016-01-04 11:27:06 -0800

    pairing

--- rps.rb
+++ rps.rb
@@ -9,11 +9,11 @@ class RPSGame
   def winner(player1, move1, player2, move2)
     if move1 == :rock && move2 == :scissors ||
         move1 == :scissors && move2 == :paper ||
-        move1 = :paper && move2 == :rock
+        move1 == :paper && move2 == :rock
       return player1
     elsif move2 == :rock && move1 == :scissors ||
         move2 == :scissors && move1 == :paper ||
-        move2 = :paper && move1 == :rock
+        move2 == :paper && move1 == :rock
       return player2
     else
       return "draw"
@@ -21,19 +21,48 @@ class RPSGame
   end
 
   def game_loop()
-    
+       player1 = @players[player_index]
+       move1 = player1.play
+       
+       switch_player
+       
+       player2 = @players[player_index]
+       move2 = player2.play
+
+       puts winner(player1,move1,player2,move2)
+       
   end
+
+  def switch_player
+      player_index == 0? (self.player_index = 1):(self.player_index = 0)
+  end
+    
 end
 
 class Player
   attr_accessor :type, :name
-
+  
   def initialize(type,name)
     @type = type
     @name = name
   end
 
   def play
-    [:rock,:paper,:scissors].sample
+    choices =  [:rock,:paper,:scissors]
+    if @type == "ai"
+       move = choices.sample
+       puts "Computer has selected its move: #{move}"
+    else 
+
+       puts "Enter your move (rock,paper or scissors):"
+       move = gets.chomp.to_sym
+       if !choices.include?(move)
+          self.play
+       end
+    end
+    move   
   end
 end
+
+game = RPSGame.new
+game.game_loop
