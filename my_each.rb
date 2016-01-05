cfa5e526d166860b3753ec9f74405c9cc7ac45da
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-04 20:43:02 -0500

    added proc handling

--- my_each.rb
+++ my_each.rb
@@ -25,28 +25,45 @@ class Array
     new_arr
   end
 
-  def my_select
+  def my_select(my_proc=nil)
     new_arr = []
     self.my_each do |item|
-      if yield(item)
-         new_arr << item
-      end   
+      if block_given?
+        if yield(item)
+          new_arr << item
+        end
+      else
+        if my_proc.call(item)
+          new_arr << item
+        end
+      end
+
     end
     new_arr
   end
 
-  def my_all
+  def my_all(my_proc=nil)
     self.my_each do |item|
-      if !yield(item)
-        return false
-      end   
+      if block_given?
+        if !yield(item)
+          return false
+        end
+      else
+        if !my_proc.call(item)
+          return false
+        end
+      end
     end
     true
   end
 
-  def my_inject(acc=0)
+  def my_inject(acc, my_proc=nil)
     self.my_each do |item|
-      acc = yield(acc,item)
+      if block_given?
+        acc = yield(acc,item)
+      else
+        acc = my_proc.call(acc,item)
+      end
     end
     acc
   end
@@ -59,27 +76,32 @@ end
 #   puts num
 # end
 
- my_proc = Proc.new {|num| num * 3}   
- test_arr = [3,4,5,20].my_map(&my_proc)
- puts test_arr
+ # my_proc = Proc.new {|num| num * 3}   
+ # test_arr = [3,4,5,20].my_map(&my_proc)
+ # puts test_arr
  
- test_arr = [3,4,5,20].my_map(my_proc)
+ # test_arr = [3,4,5,20].my_map(my_proc)
 # test_arr = [3,4,5].my_map do |num|
 #   num * 3
 # end
 
-puts test_arr
+# puts test_arr
 
 #test_arr = [3,4,5,10,20].my_select do |num|
 #  num < 10
 #end
 
-#puts test_arr
-
+# my_proc = Proc.new {|num| num < 10}
+# test_arr = [3,4,5,10,20].my_select(my_proc)
+# puts test_arr
 
 # result = [3,4,5,10,20].my_all do |num|
 #   num < 10
 # end
+# puts result
+
+# my_proc = Proc.new { |num| num < 10}
+# result = [3,4,5].my_all(my_proc)
 # puts result
 
 # result = [3,4,5,1,2].my_all do |num|
@@ -90,5 +112,9 @@ puts test_arr
 # result = [3,4,10].my_inject() do |prod,num|
 #    prod + num
 #  end
+# puts result
+
+# my_proc = Proc.new {|prod,num| prod * num}
+# result = [3,4,10].my_inject(1,my_proc)
 # puts result
 
