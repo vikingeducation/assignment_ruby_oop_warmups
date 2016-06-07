336920ecb92cf6073f8b66e64fd4bb7a5b1ddf16
Joe Bernardi <joebernardi@hotmail.com>
2016-06-06 21:14:15 -0500

    refactor player cpu win message

--- rock_paper_sciccors.rb
+++ rock_paper_sciccors.rb
@@ -7,50 +7,66 @@ class RockPaperScissors
 	#instance methods will be CPU and PLAYER
 	#attr_accessor :player_move
 
-	def initialize
+	def initialize( player_move )
 		@cpu_move = ['rock', 'paper', 'scissors'].sample
+		@player_move = player_move
 	#def CPU
 		#the CPU will generate Rock Paper or Scissors stored into a variable - this can be intialized
 	end
 	#end CPU
 
+	# checking for a tie
+	def tie? ( player_move )
+		if @cpu_move == player_move
+			puts "Tie! Both players had #{@cpu_move}"
+		end
+	end
+
+	#output upon player victory
+	def player_wins
+
+		puts "Player wins! #{@player_move} beats #{@cpu_move}"
+
+	end
+
+	#output upon CPU victory
+	def cpu_wins
+
+		puts "CPU wins! #{@cpu_move} beats #{@player_move}"
+
+	end
 
 	# set up a method to check the player and CPU moves
 	def outcome( player_move )
 		if @cpu_move == "rock"
-
-			if player_move == "rock"
-				puts "Tie! Both players had #{@cpu_move}"
-
-			elsif player_move == "paper"
-				puts "Player wins! #{player_move} beats #{@cpu_move}"
-
+			if player_move == "paper"
+				player_wins
 			elsif player_move == "scissors"
-				puts "CPU wins! #{@cpu_move} beats #{player_move}"
+				cpu_wins
 			end
 
 		elsif @cpu_move == "paper"
 
 			if player_move == "rock"
-				puts "CPU wins! #{@cpu_move} beats #{player_move}"
 
-			elsif player_move == "paper"
-				puts "Tie! Both players had #{@cpu_move}"
+				cpu_wins
 
 			elsif player_move == "scissors"
-				puts "Player wins! #{player_move} beats #{@cpu_move}"
+
+				player_wins
+
 			end
 
 		elsif @cpu_move == "scissors"
 
 			if player_move == "rock"
-				puts "Player wins! #{player_move} beats #{@cpu_move}"
+
+				player_wins
 
 			elsif player_move == "paper"
-				puts "CPU wins! #{@cpu_move} beats #{player_move}"
 
-			elsif player_move == "scissors"
-				puts "Tie! Both players had #{@cpu_move}"
+				cpu_wins
+
 			end
 
 		end
@@ -65,7 +81,8 @@ end
 puts "Please enter rock, paper or scissors"
 player_move = gets.strip.downcase
 
-new_game = RockPaperScissors.new
+new_game = RockPaperScissors.new( player_move )
 
+new_game.tie?(player_move)
 new_game.outcome(player_move)
 
