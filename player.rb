5e26fcad88360969079817c493a78acb4d035970
Conor Fischer <conor.fischer@gmail.com>
2016-06-18 23:03:40 -0500

    Refactored Choose Item to return status

--- player.rb
+++ player.rb
@@ -1,26 +1,34 @@
 class Player
-	@@item_list = ['rock','paper','scissors','quit']
+	@@item_list = {0 => 'rock',1 =>'paper',2 =>'scissors',3 => 'quit'}
 
 	def initialize()
-		@item = nil
 	end
 
 	def choose_item(item=nil)
+		status = "FAIL"
+		message = nil
+		data = nil
+
 		case item
-		when 0
+		when 0,1,2
 			@item = @@item_list[item]
-		when 1
-			@item = @@item_list[item]
-		when 2
-			@item = @@item_list[item]
+
+			status = "SUCCESS"
+			data = item
+			message = "ITEM_CHANGED"
 		else
-			@item = "INVALID_ITEM"
+			@item, message = "INVALID_ITEM"
 		end
+
+		data = @item
+		response = { status: status, message: message, data: data }
+		response
 	end
 end
 
 class Computer < Player
 	def initialize()
+		@name = "Computer"
 	end
 
 	def choose_item
@@ -30,9 +38,13 @@ class Computer < Player
 end
 
 class Human < Player
-	def initialize()
+	def initialize(name)
+		@name = "Player #{name}"
 	end
 
-
+	def choose_item
+		item = rand(0..2)
+		super(item)
+	end
 end
 
