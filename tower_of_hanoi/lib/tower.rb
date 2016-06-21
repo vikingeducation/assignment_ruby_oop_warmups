f2c13441d229776f32660e6272f2112cc4300e8a
Conor Fischer <conor.fischer@gmail.com>
2016-06-21 00:51:17 -0500

    Add Initialize & disk_push to tower

--- tower_of_hanoi/lib/tower.rb
+++ tower_of_hanoi/lib/tower.rb
@@ -1,3 +1,18 @@
 class Tower
+	attr_reader :name , :disk_set, :row_size
 
+	def initialize(name, disk_set=[])
+		@name = name
+		@disk_set = disk_set
+		@row_size = disk_set.size
+	end
+
+	def disk_push(disk)
+		@disk_set << disk
+		@row_size = disk_set.size
+	end
+
+	def disk_pop()
+
+	end
 end
