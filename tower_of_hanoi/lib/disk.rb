f38a25ac7c0555e3d8793e1ecf9679edc50b9255
Conor Fischer <conor.fischer@gmail.com>
2016-06-22 22:34:32 -0500

    Re-Intialize file structure

--- tower_of_hanoi/lib/disk.rb
+++ tower_of_hanoi/lib/disk.rb
@@ -1,12 +1,3 @@
 class Disk
-	attr_reader :size
-
-	def initialize(disk_size)
-		@size = disk_size
-	end
-
-	def compare_size(compared_size)
-		return size - compared_size
-	end
 
 end
--- tower_of_hanoi/spec/disk_spec.rb
+++ tower_of_hanoi/spec/disk_spec.rb
@@ -1,24 +1,5 @@
-require_relative '../lib/disk.rb'
+require "toh"
 
-describe "Disk" do
-	before do
-		@disk_1 = Disk.new(1)
-		@disk_2 = Disk.new(2)
-		@disk_3 = Disk.new(3)
-		@disk_4 = Disk.new(4)
-	end
+describe Disk do
 
-	it "disk size init with creation" do
-		expect(@disk_1.size).to eq(1)
-		expect(@disk_2.size).to eq(2)
-		expect(@disk_3.size).to eq(3)
-		expect(@disk_4.size).to eq(4)
-	end
-
-	it "Compare Disk size to other disk size" do
-		expect(@disk_1.compare_size(@disk_2.size)).to eq(-1)
-		expect(@disk_1.compare_size(@disk_2.size)).not_to eq(0)
-		expect(@disk_3.compare_size(@disk_2.size)).to eq(1)
-		expect(@disk_4.compare_size(@disk_1.size)).to eq(3)
-	end
 end
