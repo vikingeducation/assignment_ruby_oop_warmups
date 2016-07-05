c73bbc70f139aa3ed8d5a6460e48ea2e71aab18a
Vishal Lama <vishal.lama@gmail.com>
2016-07-05 16:10:51 -0600

    Add Tower and Disc classes

--- tower.rb
+++ tower.rb
@@ -1,3 +1,8 @@
+# until winning condition
+#   ask for move
+#   try to make the move
+#   if move didn't work, print error
+
 class Disc
   attr_reader :size
 
@@ -9,10 +14,65 @@ end
 class Tower
 
   def initialize(height)
-    add_disc
+    @stack = []
+    add_discs(height)
+  end
+
+  def add_discs(height)
+    height.downto(1) do |size|
+      @stack << Disc.new(size)
+    end
+  end
+
+  def render
+    @stack.each do |disc|
+      print disc.size.to_s + " "
+    end
   end
 end
 
 class Game
+  def initialize
+    @towers = []
+  end
 
+  def play
+    get_name
+    get_height
+    setup_board
+    until game_over? do
+      render_board
+      make_move
+    end
+    puts "Congratulations! You won! You are amazing!"
+  end
+
+  def get_name
+    puts "What is your name?"
+    @name = gets.chomp
+  end
+
+  def get_height
+    height = 0
+    until height > 0 do
+      puts "What is the height?"
+      height = gets.chomp.to_i
+    end
+    @height = height
+  end
+
+  def setup_board
+    @towers << Tower.new(@height) << Tower.new(0) << Tower.new(0)
+  end
+
+  def render_board
+    @towers.each do |tower|
+      tower.render
+      puts
+    end
+  end
+
+  def game_over?
+    false
+  end
 end
