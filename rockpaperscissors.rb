3dded237573a4486762520583405eba4f618cb0c
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-05 12:00:19 -0700

    refactored RPS.rb

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -1,11 +1,15 @@
 class RockPaperScissors
-
+  OPTIONS = {
+    'paper' => 'rock',
+    'rock' => 'scissor',
+    'scissor' => 'paper'
+  }
   def initialize
     @player = Player.new
     @computer = ComputerPlayer.new
     @computer_move = ""
   end
-  
+
   def play
 
     puts "Welcome to RPC"
@@ -13,7 +17,7 @@ class RockPaperScissors
     win_flag = nil
     while win_flag.nil?
       win_flag = player_win?
-      if win_flag 
+      if win_flag
         puts "Great job you win, computer guessed #{@computer_move}"
       elsif win_flag == false
         puts "You suck, computer wins with #{@computer_move}..."
@@ -24,52 +28,46 @@ class RockPaperScissors
   end
 
 
-  
+
   def player_win?
     @computer_move = @computer.guess
-    
-    case @player.guess
-
-      when 'scissor'
-        return true if @computer_move == "paper"
-        return false if @computer_move == "rock"
-      when 'rock'
-        return true if @computer_move == "scissor"
-        return false if @computer_move == "paper"
-      when 'paper'
-        return true if @computer_move == "rock"
-        return false if @computer_move == "scissor"
-      
-      else
-        puts "wrong move, try again..."
-      end
+    player_move = @player.guess
+    # binding.pry
+    if OPTIONS[player_move] == @computer_move
+      true
+    elsif player_move == @computer_move
+      puts "It's a tie, try again..."
+    else
+      false
+    end
   end
 
 end
 
 
 class ComputerPlayer
-  
+
   def guess
     computer_move = %w[rock paper scissor].sample
-    
+
     computer_move
   end
 end
 
 class Player
-  
+
   def guess
     puts "Enter your play"
-
-    until valid_move?
+    player_move = nil
+    binding.pry
+    until valid_move? player_move
       player_move = gets.chomp
     end
-
+    player_move
   end
 
-  def valid_move?
-
+  def valid_move? (move)
+    %w[rock paper scissor].include? move
   end
 end
 
