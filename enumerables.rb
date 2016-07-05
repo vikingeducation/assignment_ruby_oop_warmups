2575b24ba2a42bf124993ce75b43a8ac7930d29e
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-05 10:03:08 -0700

    finish my_map method

--- enumerables.rb
+++ enumerables.rb
@@ -10,7 +10,19 @@ class Array
     self
   end
 
+  def my_map(&block)
+    i = 0
+    new_arr = self
+    while i < length
+      new_arr[i] = yield(self[i])
+      i += 1
+    end
+
+    new_arr
+  end
 
 end
 
-p [1,2,3,4].my_each {|num| puts num}
+# p [1,2,3,4].my_each {|num| puts num}
+#
+p [1,2,3].my_map {|num| num + 1}
