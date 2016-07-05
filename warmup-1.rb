18b24a88ad50ee0e8bfac48d70f0d482e2dd31ff
Leo Ahnn <leoahnn@gmail.com>
2016-07-05 13:10:43 -0400

    finish warmup-3

--- warmup-1.rb
+++ warmup-1.rb
@@ -34,53 +34,6 @@ end
 
 puts my_benchmark(10000) { puts "hi" }
 
-class Array
-  def my_each(prc=nil)
-    if block_given?
-      self.each do |elem|
-        yield(elem)
-      end
-    else
-      self.each do |elem|
-        prc.call(elem)
-      end
-    end
 
-  end
-
-  def my_map(prc=nil)
-    mapped_array = []
-    
-    if block_given?
-      self.each do |elem|
-       mapped_array << yield(elem)
-      end
-    else
-      self.each do |elem|
-        mapped_array << prc.call(elem)
-      end
-    end
-
-    mapped_array
-  end
-
-  def my_select(prc=nil)
-    selected_array = []
-
-    if block_given?
-      self.each do |elem|
-        selected_array << elem if yield(elem)
-      end
-
-  end
-
-end
-
-p [1,2,5].my_each{ |item| puts item }
-my_proc = Proc.new{|item| puts item**2}
-
-p [1,2,5].my_each(my_proc)
-
-p [1,2,5].my_map { |item| item ** 2 }
 
 
