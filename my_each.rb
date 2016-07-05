cc05ec5f61101436a2e764c9aeb19578b19e5d11
Adrian Mui <adrian.mui@gmail.com>
2016-07-05 14:59:13 -0400

    finished blocks

--- my_each.rb
+++ my_each.rb
@@ -22,8 +22,21 @@ class Array
     print temp_arr, "\n"
   end
 
+  def my_all?(&block)
+    self.my_each {|num| yield(num) ? next : (return false)}
+    true
+  end
 
+  def my_inject(start_num=0, &block)
+    temp_num = start_num
+    self.my_each{|num| temp_num += yield(0, num)}
+    temp_num
+  end
 end
 
 # my_proc = Proc.new{|item| item.even?}
-[1,2,5,4, 8, 9].my_select{|item| item.even?}
+# [1,2,5,4, 8, 9].my_select(&my_proc)
+
+# my_proc = Proc.new{|item| item.even?}
+# puts [2,4,6].my_all?(&my_proc)
+puts [1,2,5].my_inject(0){|memo, item| memo + item }
