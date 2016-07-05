5de8ea1604f69f9af55d142bad1077ec0326e9bb
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-05 12:22:37 -0500

    solved warmup 3

---
+++ warmup_3.rb
@@ -0,0 +1,60 @@
+class Array
+
+	def my_each(&block)
+
+		length = self.length
+
+		i = 0
+
+		while i < self.length
+			block.call(self[i])
+			i += 1
+
+		end
+
+	end
+
+
+
+	def my_map(&block)
+
+		return_array = []
+
+		self.my_each do |element|
+			return_array << block.call(element)
+		end
+		return return_array
+	end
+
+
+	def my_select(&block)
+
+		return_array = []
+
+		self.my_each do |element|
+			return_array << element if block.call(element)
+		end
+		return return_array
+	end
+
+
+
+
+
+
+end
+
+
+
+
+
+
+
+arry = [1,3,4,5]
+
+p arry.my_select{|num| num.even?}
+
+# p arry.my_map{|num| num*num}
+
+# arry.my_each{|element| puts element} 
+
