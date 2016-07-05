82a1a82b944e58d40dd0a18c0a7093ef0cdb570f
Leo Ahnn <leoahnn@gmail.com>
2016-07-05 13:19:26 -0400

    startup RPS

---
+++ rps.rb
@@ -0,0 +1,37 @@
+# RPS class
+#   initialize (players or computer)
+#   play until win
+#   win condition
+  # player class
+  # computer class
+
+
+class RPS
+  def initialize
+  end
+
+  def play
+    until
+  end
+
+  def win?
+  end
+
+  def tie?
+  end
+
+
+end
+
+class Player
+  def move
+    
+  end
+
+end
+
+class Computer < Player
+  def move
+
+  end
+end
