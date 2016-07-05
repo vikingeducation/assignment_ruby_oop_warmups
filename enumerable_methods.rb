d33427d9e920c28c5554a36bf62ab8975f8f308b
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-05 10:58:01 -0700

    add my_all and my_select

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -27,4 +27,41 @@ class Array
     end
     return_array
   end
+
+  def my_select(my_proc = nil)
+    return self.to_enum if !block_given? && my_proc.nil?
+    return_array = []
+    self.my_each do |item|
+      if block_given?
+        return_array << item if yield(item)
+      elsif my_proc
+        return_array << item if my_proc.call(item)
+      end
+    end
+    return_array
+  end
+
+  def my_all?(my_proc = nil)
+    self.my_each do |item|
+      if block_given?
+        return false unless yield(item)
+      elsif my_proc
+        return false unless my_proc.call(item)
+      else
+        return false if !item
+      end
+    end
+
+    true
+  end
+
+  def my_inject(*args, &block)
+    if args.length == 0
+      start = self.first
+      method = block
+    elsif args.length == 1
+      if args.first.is_a? Symbol
+        start = self.first
+        method = args.first.to_proc
+  end
 end
