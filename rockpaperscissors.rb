9b806210f568f91ad2fc668cb3cc9e90a6f2da48
schoetlr <srlrs20020@gmail.com>
2016-07-05 14:35:02 -0400

    RPC refactoring

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -5,35 +5,41 @@ class RockPaperScissors
     @computer = ComputerPlayer.new
     @computer_move = ""
   end
-  #play
+  
   def play
 
     puts "Welcome to RPC"
     puts "Player move..."
-    win_flag = player_win?
+    win_flag = nil
     while win_flag.nil?
-      if win_flag
-        puts "Great job you win"
-      else
+      win_flag = player_win?
+      if win_flag 
+        puts "Great job you win, computer guessed #{@computer_move}"
+      elsif win_flag == false
         puts "You suck, computer wins with #{@computer_move}..."
+      else
+        puts "Guess again, either rock, paper, or scissor"
       end
     end
   end
 
 
-  #evaluate
+  
   def player_win?
     @computer_move = @computer.guess
+    
     case @player.guess
+
+      when 'scissor'
+        return true if @computer_move == "paper"
+        return false if @computer_move == "rock"
       when 'rock'
         return true if @computer_move == "scissor"
         return false if @computer_move == "paper"
       when 'paper'
         return true if @computer_move == "rock"
         return false if @computer_move == "scissor"
-      when 'scissor'
-        return true if @computer_move == "paper"
-        return false if @computer_move == "rock"
+      
       else
         puts "wrong move, try again..."
       end
@@ -43,21 +49,28 @@ end
 
 
 class ComputerPlayer
-  #guess
+  
   def guess
-    computer_move = %w[rock, paper, scissor].sample
-    # puts "The computer chose #{computer_move}"
+    computer_move = %w[rock paper scissor].sample
+    
     computer_move
   end
 end
 
 class Player
-  #guess
+  
   def guess
     puts "Enter your play"
-    player_move = gets.chomp
+
+    until valid_move?
+      player_move = gets.chomp
+    end
+
+  end
+
+  def valid_move?
+
   end
 end
 
-# game1 = RockPaperScissors.new
-# game1.play
+
