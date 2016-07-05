b86ad2d635fbbfa3aa221a9f9683e7d97a0a8d59
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-05 18:10:01 -0400

    Computer added to r-p-s

--- rps.rb
+++ rps.rb
@@ -14,7 +14,7 @@ class Player
 
   attr_accessor :hand
 
-  def initialize(string)
+  def initialize(string = nil)
     @hand = string
   end
 
@@ -22,15 +22,57 @@ end
 
 
 class Game
-  attr_accessor :player
+  attr_accessor :player, :choice
 
   def initialize
-    puts "We're gonna play rock, paper, scissors. Choose your hand:"
-    @player = Player.new(gets.chomp)
+    puts "We're gonna play rock, paper, scissors."
+    @player1 = Player.new
+    @computer = Computer.new
   end
 
+  def get_hand
+    puts "What do you want to choose?"
+    @player1.hand = gets.chomp.downcase
+  end
+
+def check_round(choice1, choice2)
+    if choice1 == "rock" && choice2 == "paper"
+      puts "#{choice2} wins!"
+      return true
+    elsif choice1 == "paper" && choice2 == "rock"
+      puts "#{choice1} wins!"
+      return true
+    elsif choice1 == "rock" && choice2 == "scissors"
+      puts "#{choice1} wins!"
+      return true
+    elsif choice1 == "scissors" && choice2 == "rock"
+      puts "#{choice1} wins!"
+      return true
+    elsif choice1 == "rock" && choice2 == "rock"
+      puts "No one won :("
+        return true
+    elsif choice1 == "paper" && choice2 == "scissors"
+      puts "#{choice2} wins!"
+      return true
+    elsif choice1 == "scissors" && choice2 == "paper"
+      puts "#{choice1} wins!"
+      return true
+    elsif choice1 == "paper" && choice2 == "paper"
+      puts "No one wins :("
+        return true
+    elsif choice1 == "scissors" && choice2 == "scissors"
+      puts "No one wins :("
+        return true
+    end
+  end
+
+  def play
+    self.get_hand
+    self.check_round(@player1.hand, @computer.hand)
+  end
 
 
 end
 
 game = Game.new
+game.play
