156d8d9c33109b69bbe7a67b65003f8c42ad7f78
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-07-05 13:28:51 -0400

    Implement my_all? method

--- warmup_3.rb
+++ warmup_3.rb
@@ -37,24 +37,29 @@ class Array
 		return return_array
 	end
 
+	def my_all?(&block)
 
+		self.my_each do |element|
 
+			return false if block.call(element) == false
 
+		end
 
+		true
+
+	end
 
 end
 
 
 
-
-
-
-
 arry = [1,3,4,5]
 
-p arry.my_select{|num| num.even?}
+p arry.my_all? { |num| num.class == Fixnum }
+
+#p arry.my_select{|num| num.even?}
 
 # p arry.my_map{|num| num*num}
 
-# arry.my_each{|element| puts element} 
+# arry.my_each{|element| puts element}
 
