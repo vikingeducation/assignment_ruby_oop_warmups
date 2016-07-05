25678564697f752d73f58330f4ea52d52d1dca47
schoetlr <srlrs20020@gmail.com>
2016-07-05 18:37:38 -0400

    making progress

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 height = 3
-# towers= [[], [], [], [], []]
+towers= [[], [], []]
 
-towers= [["o", "oooo"], ["oo"], ["ooo"]]
+#towers= [["o", "oooo"], ["oo"], ["ooo"]]
 
 # (1..height).each do |i|
 #   towers[0] << ("o"*i).center(height+2)
@@ -10,8 +10,15 @@ towers= [["o", "oooo"], ["oo"], ["ooo"]]
 # towers[1][0], towers[0][0] = towers[0][0], " "*(height+2)
 
 # puts "#{towers[0][0]}#{towers[1][0]}"
-print towers
-puts "#{(("-"*height).center(height+2))*3}"
-bottom_row = ""
-("1".."3").each {|ele| bottom_row += ele.center(height+2)}
-puts "#{bottom_row}"
+#print towers
+#puts "#{(("-"*height).center(height+2))*3}"
+#bottom_row = ""
+#("1".."3").each {|ele| bottom_row += ele.center(height+2)}
+#puts "#{bottom_row}"
+
+
+(1..height).each do |i|
+  towers[0] <<  ("o" * i)
+end
+
+puts towers
