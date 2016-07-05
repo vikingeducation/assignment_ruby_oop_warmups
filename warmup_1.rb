962349dea84b0b54559cad9072442728271d65a1
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-05 11:39:53 -0500

    solved fib

--- warmup_1.rb
+++ warmup_1.rb
@@ -28,4 +28,16 @@ def fibs(nth_index)
 
 end
 
-p fibs 1
+p fibs 
+
+
+def fibonacci(num)
+	result = [1]
+	current = 1
+	until num == 1
+		result.push(current)
+		current += result[result.length-2]
+		num -= 1
+	end
+	return result
+end
