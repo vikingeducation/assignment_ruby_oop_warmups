fba368507be193aae06938012c59b32188b5990f
Visiona <darbis@gmail.com>
2016-09-26 21:13:47 +0100

    Created my_benchmark method to measure time of the block processing

--- fibs.rb
+++ fibs.rb
@@ -1,12 +1,6 @@
 def fibs(x)
   first_fibs = [0, 1]
   arr_fibs = []
-  x.times do |iterate|
-    if iterate < 2
-      arr_fibs << first_fibs[iterate]
-    else
-      arr_fibs << (arr_fibs[iterate-1] + arr_fibs[iterate-2])
-    end
-  end
+  x.times {|iterate| iterate < 2 ? arr_fibs << first_fibs[iterate] : arr_fibs << (arr_fibs[iterate-1] + arr_fibs[iterate-2])}
   arr_fibs
 end
---
+++ my_benchmark.rb
@@ -0,0 +1,11 @@
+def my_benchmark(number_of_times)
+  start_time = Time.now
+  while number_of_times > 0
+    yield
+    number_of_times -= 1
+  end
+  puts Time.now.to_f - start_time.to_f
+end
+
+
+my_benchmark(10000) { puts "hi" }
