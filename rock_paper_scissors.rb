fab8e6ed6c92e6e9bc363c57addeea5611fe5a9d
Visiona <darbis@gmail.com>
2016-10-01 16:35:22 +0100

    Refactored code of Rock Paper Scissors game and split into files

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,65 +1,82 @@
+require_relative "HumanPlayer.rb"
+require_relative "ComputerPlayer.rb"
 
-
-class  HumanPlayer
-  def play
-    choice = ""
-    if choice != "r" || choice != "p" || choice != "s"
-      puts "Please call your selection by typing 'p' for paper, 's' for scissors or 'r' for rock"
-      choice = gets.chomp
-      self.play
-    else
-      puts "Please make correct selection."
-      self.play
-    end
-    choice
-  end
-end
-
-class ComputerPlayer
-  def play
-    choice = ["p","r","s"][rand(3)]
-    choice
-  end
-end
 
 class RPSGame
+  attr_reader :player_one_choice, :player_two_choice
 
-  def initialize(player_one)
-    player_one = HumanPlayer.new
-  end
-
-  def player_chooser
+  def initialize
     puts "Hello and welcome in Rock Paper Scissors Game"
-    puts "Press 1 to play against computer, or 2 to play with another player"
-    opponent_select = gets.chomp.to_i
-    if opponent_select == 2
-      @player_two = HumanPlayer.new
-    elsif opponent_select == 1
-      @player_two = ComputerPlayer.new
-    else
-      puts "We do not understand your selection. Please read instruction clearly."
-      player_chooser
-    end
-    @player_two
+    @player_one = HumanPlayer.new
   end
 
-
-  @str_1 = player_one.play
-  @str_2 = @player_two.play
-
-
-  def winner
-    if @str_1 == 'p' && @str_2 == 'r'
-      @str_2 = true
-      puts "Paper Wins!!"
-    elsif @str_1 == "r" && @str_2 == "s"
-      @str_1 = true
-      puts "Rock Wins!!"
-    elsif @str_1 == 's' && @str_2 == "p"
-      puts "Scissors Wins!!"
-    else
-      puts "We have a draw!!"
+  def get_opponent
+    loop do
+      puts "Press 1 to play against computer, or 2 to play with another player"
+      opponent_select = gets.chomp.to_i
+      if opponent_select == 2
+        @player_two = HumanPlayer.new
+        break
+      elsif opponent_select == 1
+        @player_two = ComputerPlayer.new
+        break
+      else
+        puts "We do not understand your selection. 
+              \ Please read instruction clearly."
+      end
     end
   end
 
+  def play
+
+    get_opponent
+
+    loop do
+      full_names = {
+        "s" => "Scissors",
+        "p" => "Paper",
+        "r" => "Rock"
+      }
+      player_one_choice = @player_one.get_decision
+      player_two_choice = @player_two.get_decision
+      puts "So player's One choice is #{full_names[player_one_choice]}"
+      puts "and player's Two choice is #{full_names[player_two_choice]}"
+      check_victory(player_one_choice, player_two_choice)
+      check_draw(player_one_choice, player_two_choice)
+      break if ending_game != "y"
+    end
+
+  end
+
+  def check_victory(player_one_choice, player_two_choice)
+    if player_one_choice == "p" && player_two_choice == "r" ||player_one_choice == "r" && player_two_choice == "p"
+      puts "Paper Wins!!!"
+      true
+    elsif player_one_choice == "p" && player_two_choice == "s" ||player_one_choice == "s" && player_two_choice == "r"
+      puts "Scissors Wins!!!"
+      true
+    elsif player_one_choice == "s" && player_two_choice == "r" ||player_one_choice == "r" && player_two_choice == "s"
+      puts "Rock Wins!!!"
+      true
+    end
+  end
+
+  def ending_game
+    puts "Would you like to play again? Please type 'y' to try again /
+          or any other key to exit the game"
+    gets.chomp
+  end
+
+  def check_draw(player_one_choice, player_two_choice)
+    if player_one_choice == player_two_choice
+      puts "Bummer, you've drawn..."
+      true
+    else
+      false
+    end
+  end
 end
+
+
+t = RPSGame.new
+t.play
