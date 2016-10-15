28eb616ab8e864807f4c1fdff611e148ff9cf72b
¨frendo <geewatt@gmail.com>
2016-10-15 18:15:57 +0100

    fix proc

--- methods.rb
+++ methods.rb
@@ -41,22 +41,26 @@ end
 
 class Array
 def my_each(proc_arg = self)
-	
+	i = 0	
 	if block_given?
-		i = 0
 		while i < self.length
 			yield (self[i])
 			i += 1
 		end
 	else
-		proc_arg.call
+		while i < self.length
+			proc_arg.call(self[i])
+			i +=1
+		end
 	end
-	self
+	return self
 end	
 end
 puts my_reverse("Blah!")
 puts fibs(8)
 puts my_benchmark(10000) {puts "hi" }
 [1,2,5].my_each { |item| puts item }
-my_proc = Proc.new{ |item| puts item ** 2}
-[1,2,5].my_each(&my_proc)
+my_proc = Proc.new do | item |
+	puts item ** 2
+end
+[1,2,5].my_each(my_proc)
