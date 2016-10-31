40610a8da0b910e2fa0d85fec4b72f24e1e420cc
James Harris <computersherpa@gmail.com>
2016-10-31 13:22:37 -0500

    1:22 PM

--- warmups.rb
+++ warmups.rb
@@ -5,22 +5,18 @@ def my_reverse(input)
     x = input_array.pop
     output_array.push(x)
   end
-  return output_array.join('')
+  output_array.join('')
 end
 
 def fibs(number)
+  return [] if number == 0
+  return [0] if number == 1
   fib_array = [0, 1]
-  if number == 0
-    return []
-  elsif number == 1
-    return [0]
-  else
-    while fib_array.length < number
-      new_number = fib_array[-2] + fib_array[-1]
-      fib_array.push(new_number)
-    end
+  while fib_array.length < number
+    new_number = fib_array[-2] + fib_array[-1]
+    fib_array.push(new_number)
   end
-  return fib_array
+  fib_array
 end
 
 def my_benchmark(number_of_times)
@@ -29,20 +25,23 @@ def my_benchmark(number_of_times)
     yield
   end
   end_time = Time.now
-  return end_time - start_time
+  end_time - start_time
 end
 
 class Array
-  def my_each
-    for index in 0...self.length
-      yield(self[index])
+  def my_each(proc=nil)
+    if block_given?
+      for index in 0...self.length
+        yield(self[index])
+      end
+    else
+      for index in 0...self.length
+        proc.call
+      end
     end
   end
 end
 
-
-
-
-
-
-
+#[1,2,5].my_each{ |item| puts item }
+my_proc = Proc.new{|item| puts item**2}
+[1,2,5].my_each(my_proc)
