3e671cd2a9f1d3bda9c8aa133d49a433cae98699
Christian Carey <christianrcarey@gmail.com>
2016-10-31 17:37:58 -0400

    refactor rock paper scissors

--- rps.rb
+++ rps.rb
@@ -1,65 +1,26 @@
 class RockPaperScissors 
+	BEATS = { 'r' => 's', 'p' => 'r', 's' => 'p' }
+	attr_accessor :p1, :p2
 
 	def initialize(two_player=false)
-		@two_player = two_player
-		@p1_score = 0
-		@p2_score = 0
-		@beats = { 'r' => 's', 'p' => 'r', 's' => 'p' }
-	end
-
-	def run
-		puts "Score: #{@p1_score} - #{@p2_score}"
-		puts "Choose your weapon:"
-		puts "Rock (R)"
-		puts "Paper (P)"
-		puts "Scissors (S)"
-		p1_choice = gets.chomp.downcase
-		if (@two_player)
-			p2_choice = gets.chomp.downcase
-		else
-			p2_choice = rand_weapon
+		@p1 = Player.new("Player 1", true)
+		if two_player == false
+			@p2 = Player.new("Computer", false)
+		elsif two_player
+			@p2 = Player.new("Player2", false)
 		end
-		winner = decide_winner(p1_choice, p2_choice)
-		end_round(winner)
+		
 	end
 
-	def rand_weapon
+	def self.rand_weapon
 		rps = ['r', 'p', 's']
 		rps.shuffle[0]
 	end
 
-	def decide_winner(p1_choice, p2_choice)
-		if @beats[p1_choice] == p2_choice
-			"Player 1"
-		elsif @beats[p2_choice] == p1_choice
-			"Player 2"
-		else
-			"Tie"
-		end
-	end
-
-	def end_round(winner)
-		unless winner == "Tie"
-			puts "Winner is #{winner}"
-		else
-			puts "Game is a tie!"
-		end
-		increment_score(winner)
-		puts "Play again? (Y/N)"
-		input = gets.chomp.downcase
-		if input == 'y'
-			run
-		elsif input == 'n'
-			quit
-		end
-	end
-
-	def increment_score(winner)
-		if winner == "Player 1"
-			@p1_score += 1
-		elsif winner == "Player 2"
-			@p2_score += 1
-		end
+	def run
+		puts "#{@p1.name}: #{@p1.score} - #{@p2.name}: #{@p2.score}"
+		round = Round.new(p1, p2, self)
+		round.run		
 	end
 
 	def quit
@@ -68,4 +29,105 @@ class RockPaperScissors
 
 end
 
+class Round
+	attr_accessor :winner
+
+	def initialize(p1, p2, game)
+		@p1 = p1
+		@p2 = p2
+		@game = game
+		@winner = nil
+	end
+
+	def run
+		get_weapon(@p1)
+		get_weapon(@p2)
+		@winner = decide_winner(@p1, @p2)
+		end_round
+	end
+
+	def get_weapon(player)
+		if player.human?
+			puts "#{player.name}, Choose your weapon:"
+			puts "Rock (R)"
+			puts "Paper (P)"
+			puts "Scissors (S)"
+			loop do 
+				input = gets.chomp.downcase
+				if valid_weapon?(input)
+					player.weapon = input
+					break
+				else
+					puts "Please enter (R/P/S)"
+				end
+			end
+		else
+			player.weapon = RockPaperScissors.rand_weapon
+		end
+	end
+
+	def decide_winner(p1, p2)
+		beats = RockPaperScissors::BEATS
+		
+		if beats[p1.weapon] == p2.weapon
+			p1
+		elsif beats[p2.weapon] == p1.weapon
+			p2
+		else
+			:tie
+		end
+	end
+
+	def end_round
+		unless @winner == :tie
+			puts "#{@winner.name} wins!"
+			@winner.increase_score
+		else
+			puts "It's a tie"
+		end
+		puts "Play again?(y/n)"
+		input = gets.chomp.downcase
+		loop do 	
+			if valid_yes_or_no?(input)
+				if input == 'y'
+					@game.run
+				elsif input == 'n'
+					@game.quit
+					break
+				else
+					puts "Please enter (Y/N)"
+				end
+			end
+		end
+	end
+
+	def valid_weapon?(weapon)
+		['r','p','s'].include?(weapon)
+	end
+
+	def valid_yes_or_no?(input)
+		['y','n'].include?(input)
+	end
+end
+
+class Player
+	attr_accessor :weapon, :name, :score, :human
+
+	def initialize(name, human)
+		@name = name
+		@human = human
+		@weapon = nil
+		@score = 0
+	end
+
+	def human?
+		human
+	end
+
+	def increase_score
+		@score += 1
+	end
+end
+
 game = RockPaperScissors.new
+game.run
