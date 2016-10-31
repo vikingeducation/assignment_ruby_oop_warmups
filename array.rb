4c51438ed35ae861e95dc9ddf7c92883aadb2896
Jessica G <gillan.jessica@gmail.com>
2016-10-31 12:20:38 -0600

    add my_map, my_select, my_all?

--- array.rb
+++ array.rb
@@ -7,14 +7,43 @@ class Array
         my_proc.call(self[num])
       end
     end
+    self
   end
 
-  def my_map
+  def my_map 
+    new_arr = []
+    self.my_each { |item| new_arr << yield(item) }
+    new_arr    
+  end
+
+  def my_select
+    new_arr = []
+    self.my_each { |item| new_arr << item if yield(item) }
+    new_arr
+  end
+
+  def my_all?
+    self.my_each { |item| return false unless yield(item) }
+    return true
+  end
+
+  def my_inject(initial_val = 0)
     
   end
 end
 
-[1,2,5].my_each { |item| puts item}
-my_proc = Proc.new { |item| puts item}
+# [1,2,5].my_each { |item| puts item}
+# my_proc = Proc.new { |item| puts item}
 
-[1,2,5].my_each my_proc
+# [1,2,5].my_each my_proc
+
+# puts [1,2,3,4].my_map { |i| i*i }
+
+puts [2,4].my_all? { |item| item.even? }
+
+
+
+
+
+
+
