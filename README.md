0a73a847e6f5fb6a04043b87349499f5765fb6de
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-10-31 10:56:32 -0700

    Add name and refine my_referse.rb

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # assignment_ruby_oop_warmups
 
 Alexa Anderson
+Catherine Kwak
--- my_reverse.rb
+++ my_reverse.rb
@@ -1,9 +1,11 @@
 def my_reverse(str)
-  result = ''
-  # str = str.to_a
-  my_arr = str.split('')
-  while !my_arr.empty?
-    result += my_arr.pop
-  end
-  result
+  # result = ''
+  # # str = str.to_a
+  # my_arr = str.split('')
+  # while !my_arr.empty?
+  #   result += my_arr.pop
+  # end
+  # result
+
+  (str.length - 1).downto(0) { |i| puts str[i] }
 end
