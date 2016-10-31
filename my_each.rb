ae110edbb7af21345f37f66d66d24cded7e46ac8
Jonathan Popenuck <john.popenuck@gmail.com>
2016-10-31 11:21:49 -0700

    finished my_each

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,14 @@
 class Array
   def my_each(my_proc)
     i = 0
-    self.length.times do
-      yield self[i]
+    length.times do
+      my_proc.call(self[i])
       i += 1
     end
     p self
   end
 end
-my_proc = {|item| puts item}
+my_proc = Proc.new{ |item| puts item }
+[1,2,5].my_each(my_proc)
+my_proc = Proc.new{ |item| puts item ** 2 }
 [1,2,5].my_each(my_proc)
