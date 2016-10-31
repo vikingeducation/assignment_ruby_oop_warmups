d55bed720f25022acf453601ffe968d7d3bfb799
Kenneth Kottenstette <kennykotten@gmail.com>
2016-10-31 16:19:52 -0700

    switch

--- ToH.rb
+++ ToH.rb
@@ -3,28 +3,43 @@ class ToH
     @peg1 = Peg.new
     @peg2 = Peg.new
     @peg3 = Peg.new
+    @board = { one: @peg1, two: @peg2, three: @peg3 }
   end
 
-  def gamelogic
-    
-    set_disks
+  def gamelogic    
+    set_discs
   end
 
-  def set_disks
-    puts "how many disks do you want?"
-    amount = gets.strip.to_s
+  def set_discs
+    puts "how many discs do you want?"
+    amount = gets.strip.to_i
     temp_hash = {}
     amount.downto(0) do |keyname|
-      temp_hash << keyname: keyname.to_i
-    end    
-
+      temp_hash[keyname] = keyname
+    end
+    temp_hash.delete(0)
+    @peg1.stack = temp_hash    
   end
+
+  def move(from,to)
+    
+    display
+  end
+
+  def is_valid?
+    #
+  end
+
+  def display
+    puts @peg1.stack ; puts @peg2.stack; puts @peg3.stack; 
+  end
+
 end
 
 class Peg
   attr_accessor :stack
   def initialize 
-    @stack = []
+    @stack = {}
   end
 end
 
@@ -35,5 +50,8 @@ class Disc
   end
 end
 
-a = new.ToH
+a = ToH.new
 puts a
+a.display
+a.set_discs
+a.display
