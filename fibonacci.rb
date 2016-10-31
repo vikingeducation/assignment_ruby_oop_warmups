7a0475a22da46922205eea02015e236f5296eb7a
Francisco Suero <suerofrancisco@yahoo.com>
2016-10-31 13:35:01 -0400

    fib sequence done

---
+++ fibonacci.rb
@@ -0,0 +1,15 @@
+def fibs(times = 1)
+  a = 0
+  b = 1
+  sequence = [0, 1]
+  times.downto(3) do |i|
+    c = a + b
+    sequence.push(c)
+    a = b
+    b = c
+  end
+  puts sequence
+  return sequence
+end
+
+fibs(8)
