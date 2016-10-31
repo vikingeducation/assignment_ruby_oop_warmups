b30ceb4c9f3662198ef361d52d1d3c2bb8f819a3
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-10-31 16:55:15 -0700

    Finished first version

--- rps.rb
+++ rps.rb
@@ -1,8 +1,26 @@
+# player
+  # players make moves
+  # helper: valid?
+
+# game
+  # constant comparison hash
+    # rock
+    # paper
+    # scissors
+  # initialize players
+    # new instance of each
+      # human or computer
+  # play
+    # getting player input <--> game calls player.choice
+    # check winner
+  # helper: check winner, if inputs are same, return nil, continue while loop
+  # helper: render
+
+
 # (eventually 2) players
 class Player
 
   CHOICES = ["r", "p", "s"]
-
 
   def initialize(human=false)
     @human = human
@@ -11,39 +29,19 @@ class Player
   # players make moves
   def choice
     puts "Enter 'r', 'p', or 's' to make your choice:"
-    @input = @human ? gets.chomp.downcase : CHOICES.sample
-    choice unless is_valid?(@input)
-    @input
+    input = @human ? gets.chomp.downcase : CHOICES.sample
+    input = choice unless is_valid?(input)
+    input
   end
 
   def is_valid?(input)
-    CHOICES.include?(@input)
+    CHOICES.include?(input)
   end
 end
 
-
-
-# player
-  # players make moves
-  # helper: valid?
-
-# game
-  # constant comparison hash
-    # r
-    # paper
-    # scissors
-  # initialize players
-    # new instance of each
-      # human or computer
-  # play
-    # getting player input <--> game calls player.choice 
-    # check winner
-  # helper: check winner, if inputs are same, return nil, continue while loop
-  # helper: render
-
 # game class
 class Game
-  COMPARISONS = {:r => :s, :s => :p, :p => :r}
+  COMPARISONS = {"r" => "s", "s" => "p", "p" => "r"}
 
   def initialize(player1, player2)
     @winner = nil
@@ -55,28 +53,31 @@ class Game
   def play
     while !@winner
       # get player choices
-      @human_input = @player1.choice
-      @computer_input = @player2.choice
+      human_input = @player1.choice
+      computer_input = @player2.choice
 
       # check for winner
       @winner = check_winner(human_input, computer_input)
 
       # render
-      render(@winner)
+      render(human_input, computer_input)
     end
   end
 
+
   def check_winner(human_input, computer_input)
-    return :human if COMPARISONS[human_input] == computer_input
-    return :computer if COMPARISONS[computer_input] == human_input
+    puts "human says: #{human_input}"
+    puts "computer says: #{computer_input}"
+    return "human" if COMPARISONS[human_input] == computer_input
+    return "computer" if COMPARISONS[computer_input] == human_input
     nil
   end
 
-  def render(winner)
-    puts "You chose #{@human_input}."
-    puts "The computer chose #{@computer_input}."
-    if winner
-      puts "The winner is #{winner}."
+  def render(human_input, computer_input)
+    puts "You chose #{human_input}."
+    puts "The computer chose #{computer_input}."
+    if @winner
+      puts "The winner is #{@winner}."
     else
       puts "No winner yet!"
     end
@@ -84,4 +85,4 @@ class Game
 
 end
 
-Game.new(Player.new(human: true), Player.new)
+Game.new(Player.new(human: true), Player.new).play
