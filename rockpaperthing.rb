9ceed75f39c6a4c309441e502f7ac503cdb76637
Kenneth Kottenstette <kennykotten@gmail.com>
2016-10-31 14:59:23 -0700

    finish rockpaperthing.rb

--- rockpaperthing.rb
+++ rockpaperthing.rb
@@ -7,10 +7,10 @@ class Game
     move = @moves_array[rand(3)]
   end
 
-  def user_move
+  def user_move(player='Player 1')
     go_on = true
     while go_on == true
-      puts "rock, paper, scissors?" 
+      puts "#{player}: rock, paper, scissors?" 
       usermove = gets.strip.downcase
       unless @moves_array.include?(usermove) == true 
         puts "invalid input"
@@ -26,21 +26,21 @@ class Game
       puts "Draw"
     elsif player1_move == "rock"
       if player2_move == "paper"
-        puts "you lose"
+        puts "Player 2 wins"
       else 
-        puts "you win"
+        puts "Player 1 wins"
       end
     elsif player1_move == "scissors"
       if player2_move == "rock"
-        puts "you lose"
+        puts "Player 2 wins"
       else 
-        puts "you win"
+        puts "Player 1 wins"
       end
     elsif player1_move == "paper"
       if player2_move == "scissors"
-        puts "you lose"
+        puts "Player 2 wins"
       else 
-        puts "you win"
+        puts "Player 1 wins"
       end
     end
   end
@@ -51,9 +51,9 @@ class Game
       u_move = user_move
       game_logic(u_move, comp_move)
     elsif players == 2
-      comp_move = computer_move
-      u_move = user_move
-      game_logic(u_move, comp_move)
+      player1_move = user_move
+      player2_move = user_move
+      game_logic(player1_move, player2_move)
     else
       puts "Invalid input, mass multiplayer not supported"
     end
@@ -62,4 +62,4 @@ class Game
 end
 
 firstgame = Game.new
-firstgame.play
+firstgame.play(2)
