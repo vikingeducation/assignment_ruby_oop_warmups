f8863eece863368c43305f5970a37d51040e6f46
Sampson Crowley <sampsonsprojects@gmail.com>
2016-10-31 20:38:33 -0600

    factory methods for game difficulty

--- tower_of_hanoi/tower_of_hanoi.rb
+++ tower_of_hanoi/tower_of_hanoi.rb
@@ -1,3 +1,18 @@
 require './tower_controller'
 
-TowerController.new
+class Tower
+  def self.hard
+    new(8)
+  end
+  def self.medium
+    new(6)
+  end
+  def self.easy
+    new(4)
+  end
+  def initialize level=2
+    TowerController.new level
+  end
+end
+
+Tower.easy
