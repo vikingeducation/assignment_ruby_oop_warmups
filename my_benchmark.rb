12e1ba29d86a757d954da6d8ccf7495a229d44bd
andie5 <andieasa@googlemail.com>
2016-12-21 19:51:12 +0000

    measuring the time taken to run a block

--- my_benchmark.rb
+++ my_benchmark.rb
@@ -1,11 +1,10 @@
 # Write a method, my_benchmark, that takes a parameter number_of_times and a block and returns how long that block took to run that block, that many times. Remember, you can get the current time using Time.now, and you can subtract one time from another to get a duration in seconds.
 
-def my_benchmark(counter, op_block={})
+def my_benchmark(counter)
   start = Time.now
 
-  counter.times { op_block }
-
+  counter.times { yield }
   duration = Time.now - start
 end
 
-p my_benchmark(100000000) { puts "hi" }
+p my_benchmark(100) { puts "hi" }
