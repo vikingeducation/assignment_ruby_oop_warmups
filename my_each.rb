1322b68882e7dd8b265c2117914a2ad228286910
Chris Reed <cjr6439@gmail.com>
2017-01-18 15:27:45 -0600

    Added my_inject, added my_each_with_index method to my_each

--- my_each.rb
+++ my_each.rb
@@ -16,4 +16,23 @@ class Array
       end
     end
   end
+
+  def my_each_with_index(proc=nil)
+
+    if block_given?
+      index = 0
+      while index < self.length
+        yield self[index], index
+        index +=1
+      end
+    else
+      index = 0
+      while index < self.length
+        proc.call(self[index], index)
+        index +=1
+      end
+    end
+  end
+
+
 end
