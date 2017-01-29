e23345c293943e7269d598bf4ed2c2021f9b3a19
Roy Chen <roychenlei@gmail.com>
2017-01-29 15:06:03 +0800

    finished fibs method

--- warmup.rb
+++ warmup.rb
@@ -7,3 +7,20 @@ def my_reverse(string)
 
   output.join
 end
+
+def fibonacci(number)
+  if number == 1
+    return 0
+  elsif number == 2
+    return 1
+  else
+    return fibonacci(number - 1) + fibonacci(number - 2)
+  end
+end
+
+def fibs(numbers)
+  result = []
+  1.upto(numbers) { |number| result << fibonacci(number) }
+
+  result
+end
