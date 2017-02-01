f84eba198c134c353bee6a5cebe02ab9617a56e4
Roy Chen <roychenlei@gmail.com>
2017-02-01 10:39:55 +0800

    initial pseudocode, class prototyping

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,46 @@
+# Rock Paper Scissors
+
+# Classes required:
+
+# 1. A RockPaperScissors class - each instance represents a game of RPS
+# 2. A Player class - each instance represents a human player
+# 3. A Computer class - each instance represents a computer player
+
+# Methods required:
+# 1. RockPaperScissors class:
+# - method to check if game is over
+# - method to check if a game is single-player or two-player
+# - method to check who won in a given round
+# - method to quit game
+# - method to restart game
+# - method to display current score
+# - method to display instructions
+# - method to display who won if game is over
+# - method to play game
+
+# 2. Player class:
+# - method to get input from player
+# - method to check if a move provided by a human player is valid
+# - method to get/set name of player
+
+# 3. Computer class:
+# - method to randomly generate move
+
+# Pseudocode:
+
+# 1. Prompt player to make a move.
+# 2. Check whether the move is one of rock, paper, or scissors.
+# 3. Retry if it's not.
+# 4. Computer makes a random selection from rock, paper, or scissors.
+# 5. Check whether the player or computer wins this round.
+# 6. If it's a tie, repeat steps 1 to 5.
+# 7. Repeat the above until the desired number of rounds are met, or the player decides to quit.
+
+class RockPaperScissors
+end
+
+class Player
+end
+
+class Computer
+end
