20cb4f71f42943a4a1647e092ac230e322390531
Robert Wayne <robertjwayne@gmail.com>
2017-02-06 15:43:27 -0500

    Add choose_move method

--- oop_1_rock_paper_scissors/player.rb
+++ oop_1_rock_paper_scissors/player.rb
@@ -1,3 +1,7 @@
+require_relative 'rock.rb'
+require_relative 'paper.rb'
+require_relative 'scissors.rb'
+
 class Player
   def initialize(name)
     @name = name
@@ -10,6 +14,23 @@ class Player
 
   def move=(move)
     @move = move
+  end
+
+  def choose_move
+    puts "Please choose a move by entering 'rock', 'paper', or 'scissors'."
+    move_choice = gets.chomp.downcase
+
+    case move_choice
+      when "rock"
+        @move = Rock.new
+      when "paper"
+        @move = Paper.new
+      when "scissors"
+        @move = Scissors.new
+      else
+        puts "Invalid entry"
+        choose_move
+    end
   end
 
   def name
