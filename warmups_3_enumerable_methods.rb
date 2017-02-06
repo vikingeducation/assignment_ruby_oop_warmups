5c5486193093c60bb240678b3ac61d568c64eb70
Robert Wayne <robertjwayne@gmail.com>
2017-02-06 12:05:11 -0500

    Build my_map method

--- warmups_3_enumerable_methods.rb
+++ warmups_3_enumerable_methods.rb
@@ -1,11 +1,23 @@
 class Array
 
-  def my_each( proc = nil )
+  def my_each(proc = nil)
     if block_given?
       self.count.times { |i| yield(self[i]) }
     else
       self.count.times { |i| proc.call(self[i]) }
     end
+  end
+
+  def my_map(proc = nil)
+    map_arr = []
+
+    if block_given?
+      self.my_each { |i| map_arr.push(yield(i)) }
+    else
+      self.my_each { |i| map_arr.push(proc.call(i)) }
+    end
+
+    return map_arr
   end
 
 end
