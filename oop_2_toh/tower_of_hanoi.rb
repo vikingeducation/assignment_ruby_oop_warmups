e213eb1abc92e757fcf0ccf47eea3ffbed2bb90c
Robert Wayne <robertjwayne@gmail.com>
2017-02-06 17:32:15 -0500

    Add initialize method for TowerOfHanoi class

--- oop_2_toh/tower_of_hanoi.rb
+++ oop_2_toh/tower_of_hanoi.rb
@@ -1,3 +1,16 @@
-class TowerOfHanoi
+require_relative 'rod.rb'
 
+class TowerOfHanoi
+  def initialize(disks=3)
+    @rod_one = Rod.new
+    @rod_two = Rod.new
+    @rod_three = Rod.new
+
+    i = disks
+    disks.downto(1) do |i|
+      @rod_one.add_disk(i)
+    end
+  end
 end
+
+TowerOfHanoi.new
