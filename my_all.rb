4e9e3fcac3a28ac16e40bf483b7b927098d7ecb7
Ann Allan <ann.cristina.allan@gmail.com>
2017-02-09 11:49:02 -0500

    Add my_all? solution

---
+++ my_all.rb
@@ -0,0 +1,27 @@
+require_relative 'my_each'
+require 'pry'
+
+class Array
+  def my_all?(proc = nil, &block)
+    if block_given?
+      self.my_each do |x|
+        if block.call(x) == false
+           puts "false"
+           break
+        end
+         puts "true"
+       end
+    else
+      self.my_each do |x|
+        if proc.call(x) == false
+          puts "false"
+          break
+        end
+        puts "true"
+      end
+    end
+  end
+end
+
+my_proc = Proc.new{|item| item.even?}
+[1,2,5].my_all?(my_proc)
