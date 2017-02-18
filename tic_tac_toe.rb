36f6da7adb2e8b80f229cfd6a2dcec23bf0e694c
Chad Lucas <chad.lucas.19@gmail.com>
2017-02-18 07:26:21 -0600

    add cpu selection

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,14 +1,28 @@
+require 'pry'
 class Game
-	def start
+	def start(name)
 		#Start prompt
+		puts "Welcome #{name}"
 		puts "Select\n1: Rock \n2: Paper\n3: Scissors\n"
 		#gets user input
-		player1_choice = gets.chomp
+		player1_choice = gets.chomp.to_i
+		binding.pry
+	end
 
-		
+	def cpu_choice
+		cpu_result = rand(3) + 1
 	end
 end
 
 
+class Player
+	def initialize(name)
+		@name = name
+	end
+end
+
+player1 = Player.new("Chad")
+
 game = Game.new
-game.start
+game.start(player1)
+game.cpu_choice
