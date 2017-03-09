dd4fb4a5e5b81b00db3ddcdd25462a24dc00ba08
Brent Guffens <brentguf@users.noreply.github.com>
2017-03-09 18:28:36 +0100

    spaces > tabs

--- oop_warmup_1.rb
+++ oop_warmup_1.rb
@@ -3,19 +3,19 @@
 ## Function 1: reverse string 
 
 def my_reverse(string)
-	if string.length % 2 == 0
-		n = string.length/2
-	else
-		n = string.length/2 + 1
-	end
+  if string.length % 2 == 0
+    n = string.length/2
+  else
+    n = string.length/2 + 1
+  end
 
-	n.times do |i|
-		temp = string[i]
-		string[i] = string[-i-1]
-		string[-i-1] = temp
-	end
+  n.times do |i|
+    temp = string[i]
+    string[i] = string[-i-1]
+    string[-i-1] = temp
+  end
 
-	puts string
+  puts string
 end
 
 ## Function 2: Fibonacci
@@ -23,45 +23,45 @@ end
 ### Version 1
 
 def fibonacci(n)
-	fibo_nums = [0, 1]
+  fibo_nums = [0, 1]
 
-	(n-2).times do 
-		fibo_nums << fibo_nums[-2] + fibo_nums[-1]
-	end
+  (n-2).times do 
+    fibo_nums << fibo_nums[-2] + fibo_nums[-1]
+  end
 
-	fibo_nums
+  fibo_nums
 end
 
 ### Version 2 (recursive)
 
 def fibonacci(n)
-	fibo_nums []
+  fibo_nums []
 
-	n.times do |i|
-		fibo_nums << recursive_fibo(i)
-	end
+  n.times do |i|
+    fibo_nums << recursive_fibo(i)
+  end
 
-	fibo_nums
+  fibo_nums
 end
 
 def recursive_fibo(n)
-	if n == 0 || n == 1
-		n
-	else
-		recursive_fibo(n-1) + recursive_fibo(n-2)
-	end
+  if n == 0 || n == 1
+    n
+  else
+    recursive_fibo(n-1) + recursive_fibo(n-2)
+  end
 end
 
 # WARMUP 2: Benchmarker
 
 def my_benchmark(n)
-	start = Time.now
-	n.times do
-	 yield
-	end
-	total_time = Time.now	- start
+  start = Time.now
+  n.times do
+    yield
+  end
+  total_time = Time.now	- start
 
-	total_time
+  total_time
 end
 
 # WARMUP 3: Rebuild the Enumerable Methods
@@ -70,14 +70,14 @@ end
 
 class Array 
 
-	def my_each 
+  def my_each 
 		(self.length).times do |i|
-			yield self[i]
+      yield self[i]
 		end
 		self
-	end
+  end
 
-	def my_map
+  def my_map
 	  result = []
 
     self.my_each do |el|
@@ -85,9 +85,9 @@ class Array
     end
 
     result
-	end
+  end
 
-	def my_select
+  def my_select
 	  result = []
 
     self.my_each do |el|
@@ -95,19 +95,19 @@ class Array
     end
 
     print result
-	end
+  end
 
-	def my_all?
+  def my_all?
 
     self.my_each do |el|
       if !yield(el)
-      	false
-  			exit
+        false
+        exit
       end
     end
     true
 
-	end
+  end
 
 end
 
