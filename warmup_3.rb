9cfc0f1c9fc1dad0cb6f5d90b4b1b1bf327f052a
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-03-30 13:43:09 -0600

    my_map method done

--- warmup_3.rb
+++ warmup_3.rb
@@ -1,30 +1,40 @@
 #my_each - does the same as Ruby's each method
 class Array
   def my_each (*a_proc)
-    iteration = 0
-    until self.length == iteration
+    current_index = 0
+    until self.size == current_index
       if block_given?
-        yield self[iteration]
+        yield self[current_index]
       else
-        a_proc.call self[iteration]
+        a_proc.call self[current_index]
       end #end of if statement
-      iteration += 1
+      current_index += 1
     end #end of until
-    self
-  end #end of method
+  end #end of my_each method
+
+
+  #my_map - does same as Ruby's map method - can use my_each method to implement
+  def my_map (*proc1)
+  #returns a modified array, modified based on block or proc passed
+    new_array = []
+    self.my_each do |item|
+      new_array << yield(item)
+    end
+    new_array
+  end #end of my_map method
+
+
+
+
 end #end of class
 
-
-#test
+#tests
 my_array = Array.new([1,2,4,6])
-my_array.my_each { |item| puts item * 10}
+my_array.my_each do |item|
+  puts item * 10
+end
 my_proc = Proc.new {|item| puts item + 10}
 my_array.my_each(&my_proc)
 
-
-
-#my_map - does same as Ruby's map method - can use my_each method to implement
-def my_map
-
-
-end #end of method
+my_array.my_map {|item| puts "hello" * item}
+my_array.my_map(&my_proc)
