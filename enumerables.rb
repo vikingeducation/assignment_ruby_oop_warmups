721c58b5617db4dd413c03369c53558668be443a
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-02 17:07:07 -0500

    add validate_coordinates method

--- enumerables.rb
+++ enumerables.rb
@@ -1,16 +1,39 @@
 class Array
-def my_each(&block)
-	new_array = []
-	
-	new_array.push(yield block.call)
+	def my_each
+		i = 0
+		while i < self.length
+			yield(self[i])
+			i += 1
+	  end
+	  self
+  end
 end
 
+arr = [1,3,5]
 
-array = [1,3,4]
+arr.my_each do |n|
+	puts n
 
-array.my_each do |x|
-	puts x
-	end
-	puts array.length
+  def my_map(&block)
+  	arr = []
+  	i = 0
+  	while i < self.length
+  		arr << yield(self[i])
+  		i += 1
+  	end
+  	arr
+  end
+
+  def my_select(&block)
+  	arr = []
+  	i = 0
+  	while i < self.length
+  		if yield(self[i])
+  			arr << self[i]
+  		end
+  		i += 1
+  	end
+  end
+  
 
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,44 +1,55 @@
 require 'pry'
 class Game
+	#hash of game selection
+	@@game_selection = {1 => "Rock", 2 => "Paper", 3 => "Scissors"}
 	def start(name)
 		#Start prompt
 		puts "Welcome #{name}"
 		puts "Select\n1: Rock \n2: Paper\n3: Scissors\n"
 		#gets user input
 		@player1_choice = gets.chomp.to_i
-		#creates hash of game choices
-		game_selection = {1 => "Rock", 2 => "Paper", 3 => "Scissors"}
 		#iterates through hash 
-		game_selection.each do |key, value|
+		@@game_selection.each do |key, value|
 			#checks hash and displays the item selected
 			if @player1_choice == key
-				puts "You selected #{value}"
+				puts "You selected: #{value}"
 			end
 		end
 	end
 
 	def cpu_choice
 		#gets random number between 1-3
-		 puts @cpu_choice = rand(3) + 1
+		binding.pry
+		 @cpu_choice = rand(3) + 1
+		 #iterates through hash
+		 @@game_selection.each do |key, value|
+		 	#checks computers selection and compares it to key in hash
+		 	if @cpu_choice == key
+		 		puts "Computer selected: #{value}"
+		 	end
+		 end
 	end
 
 	def win
-		if @player1_choice == @cpu_choice
-		  puts "Draw"
-		end
+		  #puts losing combinations
+	      puts "You Lose!" if @player1_choice == 1 && @cpu_choice == 2 || @player1_choice == 2 && @cpu_choice == 3
+		  #puts wining combinations
+		  puts "You Win!" if @player1_choice == 1 && @cpu_choice == 3 || @player1_choice == 3 && @cpu_choice == 2
+		  #puts draw if computer and plater pick the same 
+		  puts "Draw!" if @player1_choice == @cpu_choice
 	end
 end
 
 
 class Player
-	def initialize(name)
-		@name = name
-	end
+	attr_accessor :name
+	
 end
 
-player1 = Player.new("Chad")
+player1 = Player.new
+player1.name = "Chad"
 
 game = Game.new
-game.start(player1)
+game.start(player1.name)
 game.cpu_choice
 game.win
