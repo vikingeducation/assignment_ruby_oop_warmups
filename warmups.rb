2dedf9097a66210ec131a06418cae7431dce8138
Aaron Garza <robertaarongarza@gmail.com>
2017-04-20 22:16:54 -0400

    Add select method

--- warmups.rb
+++ warmups.rb
@@ -34,18 +34,101 @@ end
 my_benchmark(15) 
 
 
-def my_each()
-end 
+class Array 
 
-def my_map()
+  def my_each
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+    self
+  end
+
 end
 
-def my_select()
+array = [1,2,3]
+array.my_each { |num| puts num*2 } 
+puts array.inspect
+
+
+class Array 
+
+  def my_inject(start=nil, &block) 
+    start = self[0] if start.nil? 
+    i = 0
+    total = start
+    while i < self.length
+      total = yield(total, self[i]) 
+      i += 1
+    end
+    total
+  end
+
 end
 
-def my_all?()
+array = [1,2,3]
+
+puts array.my_inject(0) do |memo, item| 
+  memo + item 
 end
 
-def my_inject()
+
+
+class Array 
+
+  def my_map(&block) 
+    i = 0
+    arr = []
+    while i < self.length
+      arr.push(yield(self[i]))
+      i += 1
+    end
+    arr
+  end
+
 end
 
+array = [1,2,3]
+array.my_map { |num| puts num*2 } 
+puts array.inspect
+
+
+
+class Array 
+
+  def my_select(&block) 
+    i = 0
+    arr = []
+    while i < self.length
+      if yield(self[i])  
+        arr.push(self[i])
+      end
+      i += 1
+    end
+    arr
+  end
+
+end
+
+array = [1,2,3,5,6,4,8,9]
+puts array.my_select { |num| num % 2 == 0} 
+
+class Array 
+
+  def my_select(&block) 
+    i = 0
+    arr = []
+    while i < self.length
+      if yield(self[i])  
+        arr.push(self[i])
+      end
+      i += 1
+    end
+    arr
+  end
+
+end
+
+array = [1,2,3,5,6,4,8,9]
+puts array.my_select { |num| num.even? } 
