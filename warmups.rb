e751f440c43f2a89e10ed8804bae071aedc5b85a
Aaron Garza <robertaarongarza@gmail.com>
2017-04-20 22:30:42 -0400

    Add OOP tic tac toe

--- warmups.rb
+++ warmups.rb
@@ -94,26 +94,6 @@ array.my_map { |num| puts num*2 }
 puts array.inspect
 
 
-
-class Array 
-
-  def my_select(&block) 
-    i = 0
-    arr = []
-    while i < self.length
-      if yield(self[i])  
-        arr.push(self[i])
-      end
-      i += 1
-    end
-    arr
-  end
-
-end
-
-array = [1,2,3,5,6,4,8,9]
-puts array.my_select { |num| num % 2 == 0} 
-
 class Array 
 
   def my_select(&block) 
@@ -132,3 +112,4 @@ end
 
 array = [1,2,3,5,6,4,8,9]
 puts array.my_select { |num| num.even? } 
+
