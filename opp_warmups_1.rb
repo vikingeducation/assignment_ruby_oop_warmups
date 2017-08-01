468ca0adbd5fff190070ca21b7d80f14457d3ea7
jucavi <jcarvil@gmail.com>
2017-08-01 23:19:35 +0200

    Add method fibs

--- opp_warmups_1.rb
+++ opp_warmups_1.rb
@@ -4,3 +4,11 @@ def my_reverse(str)
   (str.length-1).downto(0) { |index| str_reversed << str[index] }
   str_reversed
 end
+
+def fibs(order)
+  fibo = [0]
+  while fibo.length < order
+    fibo << fibo[-1]  + ( fibo[-2] == nil ? 1 : fibo[-2])
+  end
+  fibo
+end
