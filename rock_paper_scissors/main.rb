cb4f9f249671aec705a17f1a5a8f9d6c756037e6
steven4354 <steven4354@gmail.com>
2017-09-04 18:50:40 -0700

    feat: fixed the non-running issues in one player mode

--- rock_paper_scissors/main.rb
+++ rock_paper_scissors/main.rb
@@ -1,12 +1,20 @@
+require_relative "one_player.rb"
+require_relative "two_player.rb"
+
 p "Welcome to the game of rock, paper, scissors! Would you like to play against the computer or with a friend?"
+sleep(1)
 p "Press '1' to play against the computer."
+sleep(1)
 p "Press '2' to play with a friend."
 
 choice = gets.chomp
 
-if choice = "1"
+if choice == "1"
   p "You've selected to play against the computer. Let's start!"
   game = OnePlayer.new()
+  input = game.get_input
+  computer = game.computer_move
+  game.results(input, computer)
 else
   p "You've selected to play with a friend. Let's start!"
   game = TwoPlayer.new()
--- rock_paper_scissors/one_player.rb
+++ rock_paper_scissors/one_player.rb
@@ -5,8 +5,11 @@ class OnePlayer
 
   def get_input()
     p "Your move! Do you pick rock, paper or scissors?"
+    sleep(1)
     p "Press '1' for rock"
+    sleep(1)
     p "Press '2' for scissors"
+    sleep(1)
     p "Press '3' for paper"
     @input = gets.chomp
     return @input
@@ -14,13 +17,21 @@ class OnePlayer
 
   def computer_move()
     p "The computer also just selected its move as you placed your choice!"
+    sleep(1)
     @computer = (rand * 3).ceil
     @computer = @computer.to_s
-    p @computer
+    if @computer == '1'
+      p "It chose: rock"
+    elsif @computer == '2'
+      p "It chose: scissors"
+    elsif @computer == '3'
+      p "It chose: paper"
+    end
     return @computer
   end
 
   def results(input, computer)
+    sleep(2)
     if input == computer
       p "It's a tie!"
     elsif (input == '1' && computer == '2') || (input == '2' && computer == '3') || (input == '3' && computer == '1') #player wins
@@ -28,6 +39,6 @@ class OnePlayer
     else
       p "You lose"
     end
-  end 
+  end
 
 end
