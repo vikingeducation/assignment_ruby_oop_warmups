50789995c8eec53ae0941f10ed492c64d750594b
anne <richardson.ae@gmail.com>
2017-09-05 16:13:16 -0500

    Complete fibonacci method

--- ruby_methods.rb
+++ ruby_methods.rb
@@ -8,4 +8,18 @@ def my_reverse(string)
   reversed.join
 end
 
-puts my_reverse("Blah!")
+my_reverse("Blah!")
+
+
+# Write a method that takes an integer and spits out an array of that many elements of
+# the Fibonacci Sequence, which starts with 0 and 1 and computes the next element by
+# adding the most recent two elements together.
+def fibs(input)
+  output = [0, 1]
+  while output.length < input
+    output << (output[-2] + output[-1])
+  end
+  output
+end
+
+fibs(8)
