3cbc52e5651dc8a94d7204d1d343084c832e2312
anne <richardson.ae@gmail.com>
2017-09-05 21:50:26 -0500

    Complete my_inject method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -94,3 +94,30 @@ end
 # takes a proc
 my_proc = Proc.new{|item| item.even?}
 [1,2,5].my_all?(my_proc)
+
+
+# Create a method my_inject which extends the Array class and works the
+# same as Ruby's native inject method. You can use your my_each method to
+# implement this. Make it take either a block or a proc.
+class Array
+  def my_inject(starter_value = 0, proc_argument = nil)
+    current_value = starter_value
+    self.my_each do |item|
+      if block_given?
+        current_value = yield(current_value, item)
+      else
+        current_value = proc_argument.call(current_value, item)
+      end
+    end
+    p current_value
+  end
+end
+
+# takes a block
+[1,2,5].my_inject(0) do |memo, item|
+  memo + item
+end
+
+# takes a proc
+adder = Proc.new {|memo, item| memo + item }
+[1,2,5].my_inject(0, adder)
