c0664c74ef3a2178ef1061b83e0a0d4dedae1f28
anne <richardson.ae@gmail.com>
2017-09-05 20:05:48 -0500

    Complete my_map method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -19,3 +19,26 @@ end
 # accepts a proc
 putser_proc = Proc.new { |item| puts item }
 [1,2,5].my_each(&putser_proc)
+
+
+# Create a method my_map which extends the Array class and has identical
+# functionality to Ruby's native map method. You can use your my_each method
+# to implement this. Make it take either a block or a proc.
+class Array
+  def my_map(proc_argument = nil)
+    mapped_array = []
+    self.my_each do |item|
+      block_given? ? mapped_array << yield(item) : mapped_array << proc_argument.call(item)
+    end
+    p mapped_array
+  end
+end
+
+# takes a block
+[1, 2, 5].my_map do |item|
+  item ** 2
+end
+
+# takes a proc
+squarer = Proc.new { |item| item ** 2 }
+[1, 2, 5].my_map(&squarer)
