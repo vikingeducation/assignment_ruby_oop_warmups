445505400a48e15a2d7bb164962b2ee14f6786ff
anne <richardson.ae@gmail.com>
2017-09-05 21:15:36 -0500

    Write welcome_player method

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,14 +1,27 @@
 class Game
-  attr_accessor :players
+  attr_accessor :qty_players
 
   def initialize(args)
-    @players = args[:players]
+    @qty_players = args[:qty_players]
   end
 
   def play
-    puts self.players
+    welcome_player
   end
+
+  private
+
+  def welcome_player
+    header = "Welcome to Rock, Paper, Scissors!"
+    puts "-" * header.length
+    puts header
+    puts "-" * header.length
+    puts "You go head to head against the computer's randomly-selected weapon."
+    puts "The winner is the player who won the best out of 3 rounds."
+  end
+
 end
 
-game = Game.new(players: 1)
+
+game = Game.new(qty_players: 1)
 game.play
