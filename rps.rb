0d6aaf161938f85108c4ec9dafe50058aab0937d
mac718 <mac718@gmail.com>
2017-09-07 18:59:01 -0400

    Initial tortured layout of OOP rps

---
+++ rps.rb
@@ -0,0 +1,81 @@
+OPTIONS = ['rock', 'paper', 'scissors']
+
+class HumanPlayer 
+  attr_accessor :name, :wins
+  
+  def move 
+    puts "Rock, paper, or scissors?"
+    @player_move = gets.chomp 
+    
+    while !OPTIONS.include?(@player_move.downcase)
+      puts "Not an option; try again."
+      puts "Rock, paper, or scissors?"
+      @player_move = gets.chomp
+    end
+    @player_move
+  end
+end
+
+class ComputerPlayer
+  attr_accessor :name, :wins
+  
+  def move
+    @computer_move = OPTIONS.sample
+  end
+  
+  def display_move
+    puts "Computer chooses #{@computer_move}"
+  end
+end
+
+class PlayGame
+  def initialize
+    @human_player = HumanPlayer.new 
+    @computer_player = ComputerPlayer.new
+    puts "**Welcome to Rock, Paper, scissors!**"
+  end
+  
+  def player_move
+    @human_move = @human_player.move 
+  end
+  
+  def computer_move
+    @computer_move = @computer_player.move
+  end
+  
+  def display_computer_move
+    @computer_player.display_move
+  end
+  
+  def compute_winner
+    @player_wins = false
+    if @human_move == "rock" && @computer_move == 'scissors'
+      @player_wins = true
+    elsif @human_move == 'paper' && @computer_move == 'rock'
+      @player_wins = true
+    elsif @human_move == 'scissors' && @computer_move == 'paper'
+      @player_wins = true
+    end
+    
+  end
+  
+  def display_results
+    @player_wins ? (puts "Word!") : (puts "Sheeeit!")
+  end 
+  
+  def play 
+    loop do
+    player_move
+    computer_move
+    display_computer_move
+    compute_winner
+    display_results
+    puts "Play again?"
+    repeat_game = gets.chomp.downcase
+    break if repeat_game == 'no'
+  end
+  end
+end
+
+game = PlayGame.new
+game.play
