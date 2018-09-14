c833825af708233c186f9379511147c0d600ee55
Ann Allan <ann.cristina.allan@gmail.com>
2018-09-14 18:48:33 -0400

    Refactor to remove unnecessary lines

--- benchmarker.rb
+++ benchmarker.rb
@@ -3,7 +3,6 @@ def my_benchmark(number_of_times, &block)
   number_of_times.times do |function|
   end
   ending = Time.now
-  amount_time = ending - start
-  puts amount_time
+  puts (ending - start)
 end
 my_benchmark(10000){puts "hi"}
