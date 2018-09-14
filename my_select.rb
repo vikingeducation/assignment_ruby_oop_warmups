541f32fb8a4a0e940a24675131ca7b93d5e8917f
Ann Allan <ann.cristina.allan@gmail.com>
2018-09-14 19:06:31 -0400

    Refactor to remove unnecessary code

--- my_select.rb
+++ my_select.rb
@@ -1,5 +1,4 @@
 require_relative 'my_each'
-require 'pry'
 
 class Array
   def my_select(proc = nil, &block)
@@ -10,18 +9,16 @@ class Array
            new_arr << x
          end
       end
-      puts new_arr
     else
       self.my_each do |x|
         if proc.call(x)
           new_arr << x
         end
       end
-      puts new_arr
     end
   end
 end
 
 my_proc = Proc.new{|item| item.even?}
 
-[1,2,5].my_select(my_proc)
+[1, 2, 5].my_select(my_proc)
